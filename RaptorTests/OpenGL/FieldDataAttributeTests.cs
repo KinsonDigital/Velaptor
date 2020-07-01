ed268b5e8451774af19da4695ec34bfce2de545e
Calvin Wilkinson <kinsondigital@gmail.com>
2020-07-01 19:27:56 -0400

    Create custom field data attribute with unit tests

---
+++ RaptorTests/OpenGL/FieldDataAttributeTests.cs
@@ -0,0 +1,28 @@
+﻿using Raptor.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RaptorTests.OpenGL
+{
+    /// <summary>
+    /// Tests the <see cref="FieldDataAttribute"/> class.
+    /// </summary>
+    public class FieldDataAttributeTests
+    {
+        [Fact]
+        public void Ctor_WhenInvoked_CorrectlySetsPropertyValues()
+        {
+            //Arrange
+
+            //Act
+            var attribute = new FieldDataAttribute(4, 3);
+
+            //Assert
+            Assert.Equal(4u, attribute.BytesPerElement);
+            Assert.Equal(3u, attribute.TotalElements);
+            Assert.Equal(12u, attribute.TotalBytes);
+        }
+    }
+}
