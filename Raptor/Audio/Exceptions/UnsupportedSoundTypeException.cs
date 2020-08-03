da292e8732ed2b43379b32b347381abb74243483
Calvin Wilkinson <kinsondigital@gmail.com>
2020-08-03 13:12:42 -0400

    Create unit tests for SoundLoader class

---
+++ Raptor/Audio/Exceptions/UnsupportedSoundTypeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Raptor.Audio.Exceptions
+{
+    public class UnsupportedSoundTypeException : Exception
+    {
+        public UnsupportedSoundTypeException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedSoundTypeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public UnsupportedSoundTypeException()
+        {
+        }
+    }
+}
--- Raptor/Content/SoundLoader.cs
+++ Raptor/Content/SoundLoader.cs
@@ -1,45 +1,88 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Raptor.Audio;
-using Raptor.OpenAL;
+﻿// <copyright file="SoundLoader.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
 
 namespace Raptor.Content
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using Raptor.Audio;
+    using Raptor.Audio.Exceptions;
+    using Raptor.OpenAL;
+
+    /// <summary>
+    /// Loads sound content.
+    /// </summary>
     public class SoundLoader : ILoader<ISound>
     {
         private readonly IALInvoker alInvoker;
+        private readonly IAudioDeviceManager audioManager;
         private readonly IContentSource contentSource;
         private readonly ISoundDecoder<float> oggDecoder;
         private readonly ISoundDecoder<byte> mp3Decoder;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoundLoader"/> class.
+        /// </summary>
+        /// <param name="contentSource">Manages the source of the content.</param>
+        /// <param name="oggDecoder">Decodes ogg sound files.</param>
+        /// <param name="mp3Decoder">Decodes mp3 sound files.</param>
+        [ExcludeFromCodeCoverage]
         public SoundLoader(IContentSource contentSource, ISoundDecoder<float> oggDecoder, ISoundDecoder<byte> mp3Decoder)
         {
             this.alInvoker = new ALInvoker();
+            this.audioManager = AudioDeviceManager.GetInstance(this.alInvoker);
             this.contentSource = contentSource;
             this.oggDecoder = oggDecoder;
             this.mp3Decoder = mp3Decoder;
         }
 
-        internal SoundLoader(IALInvoker alInvoker, IContentSource contentSource, ISoundDecoder<float> oggDecoder, ISoundDecoder<byte> mp3Decoder)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SoundLoader"/> class.
+        /// </summary>
+        /// <param name="alInvoker">Make calls to OpenAL.</param>
+        /// <param name="audioManager">Manges the audio devices.</param>
+        /// <param name="contentSource">Manages the source of the content.</param>
+        /// <param name="oggDecoder">Decodes ogg sound files.</param>
+        /// <param name="mp3Decoder">Decodes mp3 sound files.</param>
+        internal SoundLoader(IALInvoker alInvoker, IAudioDeviceManager audioManager, IContentSource contentSource, ISoundDecoder<float> oggDecoder, ISoundDecoder<byte> mp3Decoder)
         {
             this.alInvoker = alInvoker;
+            this.audioManager = audioManager;
             this.contentSource = contentSource;
             this.oggDecoder = oggDecoder;
             this.mp3Decoder = mp3Decoder;
         }
 
+        /// <summary>
+        /// Loads a sound with the given name.
+        /// </summary>
+        /// <param name="name">The name of the sound content to load.</param>
+        /// <returns>The name of the sound content item to load.</returns>
+        /// <remarks>
+        ///     The content <see cref="name"/> can have or not have the extension of the content file to load.
+        ///     Using the file extension has no effect and the content item is matched using the name without the extension.
+        ///     The only sounds supported are .ogg and .mp3 files.
+        ///     Also, duplicate names are not aloud at the same location and this is matched agains the name without the extextsion.
+        ///
+        ///     Example: sound.ogg and sound.mp3 will result in an exception.
+        /// </remarks>
+        /// <exception cref="UnsupportedSoundTypeException">
+        ///     Thrown when a sound file extension other then .ogg and .mp3 are used.
+        /// </exception>
         public ISound Load(string name)
         {
             var extension = Path.GetExtension(name);
 
             if (!new[] { ".ogg", ".mp3" }.Contains(extension))
-                throw new Exception($"The extension $'{extension}' is not supported. Supported audio files are '.ogg' and '.mp3'.");
+                throw new UnsupportedSoundTypeException($"The extension '{extension}' is not supported.  Supported audio files are '.ogg' and '.mp3'.");
 
             return new Sound(
                 name,
                 this.alInvoker,
-                AudioDeviceManager.GetInstance(this.alInvoker),
+                this.audioManager,
                 this.oggDecoder,
                 this.mp3Decoder,
                 this.contentSource);
