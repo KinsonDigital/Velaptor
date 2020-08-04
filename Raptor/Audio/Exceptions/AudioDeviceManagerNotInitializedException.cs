e932e9b12f206d92b0a5b7515e41a77a12819b84
Calvin Wilkinson <kinsondigital@gmail.com>
2020-08-03 21:53:33 -0400

    Add unit tests for AudioDeviceManagerNotInitializedException class

--- Raptor/Audio/Exceptions/AudioDeviceManagerNotInitializedException.cs
+++ Raptor/Audio/Exceptions/AudioDeviceManagerNotInitializedException.cs
@@ -1,24 +1,44 @@
-﻿// <copyright file="AudioDeviceMangerNotInitializedException.cs" company="KinsonDigital">
+﻿// <copyright file="AudioDeviceManagerNotInitializedException.cs" company="KinsonDigital">
 // Copyright (c) KinsonDigital. All rights reserved.
 // </copyright>
 
 #pragma warning disable CA1303 // Do not pass literals as localized parameters
+#pragma warning disable CA1064 // Exceptions should be public
 namespace Raptor.Audio.Exceptions
 {
     using System;
 
-    public class AudioDeviceManagerNotInitializedException : Exception
+    /// <summary>
+    /// Occurs when the <see cref="AudioDeviceManager"/> has not been initialized.
+    /// </summary>
+    /// <remarks>This is done by invoking the <see cref="AudioDeviceManager.InitDevice(string?)"/> method.</remarks>
+    internal class AudioDeviceManagerNotInitializedException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioDeviceManagerNotInitializedException"/> class.
+        /// </summary>
         public AudioDeviceManagerNotInitializedException()
-            : base("asdf")
+            : base("The audio device manager has not been initialized.")
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioDeviceManagerNotInitializedException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
         public AudioDeviceManagerNotInitializedException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioDeviceManagerNotInitializedException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception, or a
+        ///     null reference if no inner exception is specified.
+        /// </param>
         public AudioDeviceManagerNotInitializedException(string message, Exception innerException)
             : base(message, innerException)
         {
