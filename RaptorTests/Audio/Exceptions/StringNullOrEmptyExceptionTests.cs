7ab5cc8f63939b909a4656836bf8da278f1d083d
Calvin Wilkinson <kinsondigital@gmail.com>
2020-08-03 21:58:47 -0400

    Create unit tests for StringNullOrEmptyException class

---
+++ RaptorTests/Audio/Exceptions/StringNullOrEmptyExceptionTests.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="StringNullOrEmptyExceptionTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+namespace RaptorTests.Audio.Exceptions
+{
+    using System;
+    using Raptor.Exceptions;
+    using Xunit;
+
+    public class StringNullOrEmptyExceptionTests
+    {
+        #region Constructor Tests
+        [Fact]
+        public void Ctor_WhenInvokedWithNoParam_CorrectlySetsMessage()
+        {
+            // Act
+            var exception = new StringNullOrEmptyException();
+
+            // Assert
+            Assert.Equal("The string must not be null or empty.", exception.Message);
+        }
+
+        [Fact]
+        public void Ctor_WhenInvokedWithSingleMessageParam_CorrectlySetsMesage()
+        {
+            // Act
+            var exception = new StringNullOrEmptyException("test-message");
+
+            // Assert
+            Assert.Equal("test-message", exception.Message);
+        }
+
+        [Fact]
+        public void Ctor_WhenInvokedWithMessageAndInnerException_ThrowsException()
+        {
+            // Arrange
+            var innerException = new Exception("inner-exception");
+
+            // Act
+            var deviceException = new StringNullOrEmptyException("device-exception", innerException);
+
+            // Assert
+            Assert.Equal("inner-exception", deviceException.InnerException.Message);
+            Assert.Equal("device-exception", deviceException.Message);
+        }
+        #endregion
+    }
+}
