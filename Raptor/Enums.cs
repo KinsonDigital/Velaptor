1795bdd1d584a1382870fe8ca486cd69746df4e9
Calvin Wilkinson <kinsondigital@gmail.com>
2020-11-05 16:54:13 -0500

    Remove unused code

--- Raptor/Enums.cs
+++ Raptor/Enums.cs
@@ -1,25 +1,9 @@
-﻿// <copyright file="Enums.cs" company="KinsonDigital">
+// <copyright file="Enums.cs" company="KinsonDigital">
 // Copyright (c) KinsonDigital. All rights reserved.
 // </copyright>
 
 namespace Raptor
 {
-    /// <summary>
-    /// Represents different ways that a scene should run.
-    /// </summary>
-    public enum RunMode
-    {
-        /// <summary>
-        /// This makes an scene run continuously.  Used for standard game running through frames.
-        /// </summary>
-        Continuous = 1,
-
-        /// <summary>
-        /// This gives fine control to run the game a set amount of frames at a time.
-        /// </summary>
-        FrameStack = 2,
-    }
-
     /// <summary>
     /// The different kinds of borders that a <see cref="IWindow"/> can have.
     /// </summary>
