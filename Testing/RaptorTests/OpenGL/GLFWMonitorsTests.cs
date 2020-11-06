b8ab7b524ab8e284a59c1ffa667ecc05b1e01554
Calvin Wilkinson <kinsondigital@gmail.com>
2020-11-05 20:48:21 -0500

    Add some control the the GC process to prevent inconsistent unit test failures due to pointers

--- Testing/RaptorTests/OpenGL/GLFWMonitorsTests.cs
+++ Testing/RaptorTests/OpenGL/GLFWMonitorsTests.cs
@@ -5,6 +5,9 @@
 namespace RaptorTests.OpenGL
 {
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
     using System.Numerics;
     using System.Runtime.InteropServices;
     using Moq;
@@ -17,17 +20,17 @@ namespace RaptorTests.OpenGL
     /// <summary>
     /// Tests the <see cref="GLFWMonitors"/> class.
     /// </summary>
-    public unsafe class GLFWMonitorsTests
+    public unsafe class GLFWMonitorsTests : IDisposable
     {
         private readonly Mock<IGLFWInvoker> mockGLFWInvoker;
-        private readonly VideoMode videoModeA;
-        private readonly VideoMode* videoModeHandleA;
-        private readonly VideoMode videoModeB;
-        private readonly VideoMode* videoModeHandleB;
-        private readonly Monitor monitorA;
-        private readonly IntPtr monitorHandleA;
-        private readonly Monitor monitorB;
-        private readonly IntPtr monitorHandleB;
+        private VideoMode videoModeA;
+        private VideoMode* videoModeHandleA;
+        private VideoMode videoModeB;
+        private VideoMode* videoModeHandleB;
+        private Monitor monitorA;
+        private IntPtr monitorHandleA;
+        private Monitor monitorB;
+        private IntPtr monitorHandleB;
         private readonly Mock<IPlatform> mockPlatform;
 
         /// <summary>
@@ -58,27 +61,27 @@ namespace RaptorTests.OpenGL
                 RefreshRate = 66,
             };
 
-            fixed (VideoMode* handleA = &this.videoModeA)
+            fixed (VideoMode* pVideoModeA = &this.videoModeA)
             {
-                this.videoModeHandleA = handleA;
+                this.videoModeHandleA = pVideoModeA;
             }
 
-            fixed (VideoMode* handleB = &this.videoModeB)
+            fixed (VideoMode* pVideoModeB = &this.videoModeB)
             {
-                this.videoModeHandleB = handleB;
+                this.videoModeHandleB = pVideoModeB;
             }
 
             this.monitorA = default;
             this.monitorB = default;
 
-            fixed (Monitor* handle = &this.monitorA)
+            fixed (Monitor* pMonitorA = &this.monitorA)
             {
-                this.monitorHandleA = (IntPtr)handle;
+                this.monitorHandleA = (IntPtr)pMonitorA;
             }
 
-            fixed (Monitor* handle = &this.monitorB)
+            fixed (Monitor* pMonitorB = &this.monitorB)
             {
-                this.monitorHandleB = (IntPtr)handle;
+                this.monitorHandleB = (IntPtr)pMonitorB;
             }
 
             this.mockGLFWInvoker = new Mock<IGLFWInvoker>();
@@ -95,6 +98,21 @@ namespace RaptorTests.OpenGL
 
             this.mockGLFWInvoker.Setup(m => m.GetMonitorContentScale(this.monitorHandleB))
                 .Returns(new Vector2(77, 88));
+
+            var pointers = new List<IntPtr>()
+            {
+                this.monitorHandleA,
+                this.monitorHandleB,
+                (IntPtr)this.videoModeHandleA,
+                (IntPtr)this.videoModeHandleB,
+            };
+
+            GC.KeepAlive(this.monitorA);
+            GC.KeepAlive(this.monitorB);
+            GC.KeepAlive(this.videoModeA);
+            GC.KeepAlive(this.videoModeB);
+            GC.KeepAlive(this.monitorHandleA);
+            GC.KeepAlive(this.monitorHandleB);
         }
 
         #region Constructor Tests
@@ -155,6 +173,14 @@ namespace RaptorTests.OpenGL
             // Assert
             Assert.Equal(expectedMonitorA, actual[0]);
             Assert.Equal(expectedMonitorB, actual[1]);
+        }
+
+        public void Dispose()
+        {
+            this.monitorHandleA = IntPtr.Zero;
+            this.monitorHandleB = IntPtr.Zero;
+
+            GC.Collect();
         }
         #endregion
     }
