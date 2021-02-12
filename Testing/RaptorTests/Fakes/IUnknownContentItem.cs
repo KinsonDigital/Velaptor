fb1e07c2e2ff4708b3e1512c75289e6eea6314f9
Calvin Wilkinson <kinsondigital@gmail.com>
2021-02-12 06:43:58 -0500

    Implement PR !653 Changes

--- Testing/RaptorTests/Fakes/IUnknownContentItem.cs
+++ Testing/RaptorTests/Fakes/IUnknownContentItem.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Raptor.Content;
+﻿// <copyright file="IUnknownContentItem.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
 
 namespace RaptorTests.Fakes
 {
+    using Raptor.Content;
+
+    /// <summary>
+    /// Used for testing.
+    /// </summary>
     public interface IUnknownContentItem : IContent
     {
     }
