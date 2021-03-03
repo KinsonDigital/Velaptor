cca7a2444afed689ca25898ef1cb9746c5a97727
Calvin Wilkinson <kinsondigital@gmail.com>
2021-03-03 18:20:16 -0500

    Improve AtlasTexturePathResolver and add unit tests

--- Raptor/Content/AtlasTexturePathResolver.cs
+++ Raptor/Content/AtlasTexturePathResolver.cs
@@ -11,38 +11,13 @@ namespace Raptor.Content
     /// <summary>
     /// Resolves paths to atlas texture content.
     /// </summary>
-    public class AtlasTexturePathResolver : ContentPathResolver
+    public class AtlasTexturePathResolver : TexturePathResolver
     {
-        private readonly IDirectory directory;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AtlasTexturePathResolver"/> class.
         /// </summary>
         /// <param name="directory">Manages directories.</param>
         public AtlasTexturePathResolver(IDirectory directory)
-        {
-            this.directory = directory;
-            FileDirectoryName = "Atlas";
-        }
-
-        /// <summary>
-        /// Returns the path to the texture atlas image content.
-        /// </summary>
-        /// <param name="name">The name of the content.</param>
-        /// <returns>The path to the content.</returns>
-        public override string ResolveFilePath(string name)
-        {
-            var contentDirPath = GetContentDirPath();
-
-            // Check if there are any files that match the name
-            var files = this.directory.GetFiles(contentDirPath, "*.png");
-
-            if (files.Length <= 0 || files.Any(f => f == $"{contentDirPath}{name}.png") is false)
-            {
-                throw new FileNotFoundException($"The texture atlas image file '{contentDirPath}{name}' does not exist.");
-            }
-
-            return files[0];
-        }
+            : base(directory) => FileDirectoryName = "Atlas";
     }
 }
---
+++ Testing/RaptorTests/Content/AtlasTexturePathResolverTests.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="AtlasTexturePathResolverTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace RaptorTests.Content
+{
+    using System.IO.Abstractions;
+    using Moq;
+    using Raptor.Content;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="AtlasTexturePathResolver"/> class.
+    /// </summary>
+    public class AtlasTexturePathResolverTests
+    {
+        #region Constructor Tests
+        [Fact]
+        public void Ctor_WhenInvoked_SetsFileDirectoryNameToCorrectResult()
+        {
+            // Arrange
+            var mockDirectory = new Mock<IDirectory>();
+
+            // Act
+            var resolver = new AtlasTexturePathResolver(mockDirectory.Object);
+            var actual = resolver.FileDirectoryName;
+
+            // Assert
+            Assert.Equal("Atlas", actual);
+        }
+        #endregion
+    }
+}
