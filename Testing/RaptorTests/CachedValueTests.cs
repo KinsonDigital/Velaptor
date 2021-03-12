1fe2e2483d6b13b44e5808e7fbc4df66bd3ff61a
Calvin Wilkinson <kinsondigital@gmail.com>
2021-03-11 23:35:35 -0500

    Improve value caching system as well as unit tests

--- Testing/RaptorTests/CachedValueTests.cs
+++ Testing/RaptorTests/CachedValueTests.cs
@@ -10,20 +10,78 @@ namespace RaptorTests
 
     public class CachedValueTests
     {
-        #region Prop Tests
+        #region Constructor Tests
         [Fact]
-        public void IsCaching_WhenSettingToTrue_ReturnsCorrectResult()
+        public void Ctor_WhenInvokedWithCachingOn_SetsValueInternally()
         {
             // Arrange
+            var externalSystemValue = 0;
             var cachedValue = new CachedValue<int>(
-                defaultValue: 5678,
-                getterWhenNotCaching: () => 1234,
-                setterWhenNotCaching: (value) => { })
-            {
-                IsCaching = true,
-            };
+                defaultValue: 1234,
+                getterWhenNotCaching: () => externalSystemValue,
+                setterWhenNotCaching: (value) => externalSystemValue = value,
+                isCaching: true);
 
             // Act
+            var actual = cachedValue.GetValue();
+
+            // Assert
+            Assert.Equal(1234, actual);
+            Assert.Equal(0, externalSystemValue);
+        }
+
+        [Fact]
+        public void Ctor_WhenInvokedWithCachingOff_SetsValueExternally()
+        {
+            // Arrange
+            var externalSystemValue = 0;
+            var cachedValue = new CachedValue<int>(
+                defaultValue: 1234,
+                getterWhenNotCaching: () => externalSystemValue,
+                setterWhenNotCaching: (value) => externalSystemValue = value,
+                isCaching: false);
+
+            // Act
+            var actual = cachedValue.GetValue();
+
+            // Assert
+            Assert.Equal(1234, externalSystemValue);
+        }
+        #endregion
+
+        #region Prop Tests
+        [Fact]
+        public void IsCaching_WhenGoingFromCachingOffToOn_ReturnsRecorrectResult()
+        {
+            // Arrange
+            var externalSystemValue = 0;
+            var cachedValue = new CachedValue<int>(
+                defaultValue: 1234,
+                getterWhenNotCaching: () => externalSystemValue,
+                setterWhenNotCaching: (value) => externalSystemValue = value,
+                isCaching: false);
+
+            // Act
+            cachedValue.IsCaching = true;
+            var actual = cachedValue.GetValue();
+
+            // Assert
+            Assert.Equal(1234, actual);
+        }
+
+        [Fact]
+        public void IsCaching_WhenGoingFromCachingOnToOff_ReturnsCorrectResult()
+        {
+            // Arrange
+            var externalSystemValue = 0;
+            var cachedValue = new CachedValue<int>(
+                defaultValue: 1234,
+                getterWhenNotCaching: () => externalSystemValue,
+                setterWhenNotCaching: (value) => externalSystemValue = value,
+                isCaching: true);
+
+            // Act
+            cachedValue.IsCaching = false;
             var actual = cachedValue.GetValue();
 
             // Assert
