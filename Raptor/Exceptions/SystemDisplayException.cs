cc6da1d4a408f452f7ea15f2b618e0530b1f50da
Calvin Wilkinson <kinsondigital@gmail.com>
2021-04-15 06:12:23 -0400

    Add new exception to deal with system monitor issues

---
+++ Raptor/Exceptions/SystemDisplayException.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="SystemDisplayException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Raptor.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Occurs when there is an issue with one of the system displays.
+    /// </summary>
+    public class SystemDisplayException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SystemDisplayException"/> class.
+        /// </summary>
+        public SystemDisplayException()
+            : base($"There was an issue with one of the system displays.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SystemDisplayException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public SystemDisplayException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SystemDisplayException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception,
+        ///     or a null reference if no inner exception is specified.
+        /// </param>
+        public SystemDisplayException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Testing/RaptorTests/Exceptions/SystemDisplayExceptionTests.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="SystemDisplayExceptionTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace RaptorTests.Exceptions
+{
+    using System;
+    using Raptor.Exceptions;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="SystemDisplayException"/> class.
+    /// </summary>
+    public class SystemDisplayExceptionTests
+    {
+        #region Constructor Tests
+        [Fact]
+        public void Ctor_WithNoParam_CorrectlySetsExceptionMessage()
+        {
+            // Act
+            var exception = new SystemDisplayException();
+
+            // Assert
+            Assert.Equal($"There was an issue with one of the system displays.", exception.Message);
+        }
+
+        [Fact]
+        public void Ctor_WhenInvokedWithSingleMessageParam_CorrectlySetsMesage()
+        {
+            // Act
+            var exception = new SystemDisplayException("test-message");
+
+            // Assert
+            Assert.Equal("test-message", exception.Message);
+        }
+
+        [Fact]
+        public void Ctor_WhenInvokedWithMessageAndInnerException_ThrowsException()
+        {
+            // Arrange
+            var innerException = new Exception("inner-exception");
+
+            // Act
+            var deviceException = new SystemDisplayException("test-exception", innerException);
+
+            // Assert
+            Assert.Equal("inner-exception", deviceException.InnerException.Message);
+            Assert.Equal("test-exception", deviceException.Message);
+        }
+        #endregion
+    }
+}
