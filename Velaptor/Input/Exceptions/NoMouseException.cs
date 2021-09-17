981cb5f47fdf5130d9fd03a5fa95fe01ff1d2ab4
Calvin Wilkinson <kinsondigital@gmail.com>
2021-09-17 08:26:11 -0400

    #17 - Create custom exceptions for no mouse or keyboard in system

---
+++ Velaptor/Input/Exceptions/NoMouseException.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="NoMouseException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Input.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Occurs when no mouse has been detected in the system.
+    /// </summary>
+    public class NoMouseException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoMouseException"/> class.
+        /// </summary>
+        public NoMouseException()
+            : base($"No mouse detected.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoMouseException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public NoMouseException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoMouseException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception,
+        ///     or a null reference if no inner exception is specified.
+        /// </param>
+        public NoMouseException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
