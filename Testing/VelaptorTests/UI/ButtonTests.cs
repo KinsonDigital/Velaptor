77742ad1d27f2d82f290a272ee9eb682a90540d7
Calvin Wilkinson <kinsondigital@gmail.com>
2021-09-29 13:08:52 -0400

    💎Create new Button control

---
+++ Testing/VelaptorTests/UI/ButtonTests.cs
@@ -0,0 +1,229 @@
+﻿// <copyright file="ButtonTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System;
+using System.Drawing;
+using VelaptorTests.Helpers;
+
+namespace VelaptorTests.UI
+{
+    using Moq;
+    using Velaptor.Content;
+    using Velaptor.Graphics;
+    using Velaptor.UI;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="Button"/> class.
+    /// </summary>
+    public class ButtonTests
+    {
+        private readonly Mock<IContentLoader> mockContentLoader;
+        private readonly Mock<ITexture> mockTexture;
+        private readonly Mock<ControlBase> mockLabel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ButtonTests"/> class.
+        /// </summary>
+        public ButtonTests()
+        {
+            this.mockTexture = new Mock<ITexture>();
+            this.mockTexture.SetupGet(p => p.Width).Returns(200);
+            this.mockTexture.SetupGet(p => p.Height).Returns(100);
+
+            this.mockLabel = new Mock<ControlBase>();
+
+            this.mockContentLoader = new Mock<IContentLoader>();
+            this.mockContentLoader.Setup(m => m.Load<ITexture>("button-face"))
+                .Returns(this.mockTexture.Object);
+        }
+
+        #region Prop Tests
+        [Theory]
+        [InlineData(false, 0)]
+        [InlineData(true, 200)]
+        public void Width_WhenGettingValueBeforeLoadingContent_ReturnsCorrectResult(bool loadContent, int expected)
+        {
+            // Arrange
+            var button = CreateButton();
+
+            if (loadContent)
+            {
+                button.LoadContent();
+            }
+
+            // Act
+            var actual = button.Width;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, 0)]
+        [InlineData(true, 100)]
+        public void Height_WhenGettingValueBeforeLoadingContent_ReturnsCorrectResult(bool loadContent, int expected)
+        {
+            // Arrange
+            var button = CreateButton();
+
+            if (loadContent)
+            {
+                button.LoadContent();
+            }
+
+            // Act
+            var actual = button.Height;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+        #region Method Tests
+        [Fact]
+        public void LoadContent_WhenInvoked_LoadsContent()
+        {
+            // Arrange
+            var button = CreateButton();
+
+            // Act
+            button.LoadContent();
+
+            // Assert
+            this.mockContentLoader.Verify(m => m.Load<ITexture>("button-face"), Times.Once);
+            this.mockLabel.Verify(m => m.LoadContent(), Times.Once);
+        }
+
+        [Fact]
+        public void Render_WithNoLoadedContentButVisible_DoesNotRender()
+        {
+            // Arrange
+            var mockSpriteBatch = new Mock<ISpriteBatch>();
+            var button = CreateButton();
+            button.Visible = true;
+
+            // Act
+            button.Render(mockSpriteBatch.Object);
+
+            // Assert
+            mockSpriteBatch.Verify(m =>
+                m.Render(It.IsAny<ITexture>(),
+                             It.IsAny<int>(),
+                             It.IsAny<int>(),
+                             It.IsAny<Color>()), Times.Never);
+        }
+
+        [Fact]
+        public void Render_WithLoadedContentButInvisible_DoesNotRender()
+        {
+            // Arrange
+            var mockSpriteBatch = new Mock<ISpriteBatch>();
+            var button = CreateButton();
+            button.Visible = false;
+            button.LoadContent();
+
+            // Act
+            button.Render(mockSpriteBatch.Object);
+
+            // Assert
+            mockSpriteBatch.Verify(m =>
+                m.Render(It.IsAny<ITexture>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<Color>()), Times.Never);
+        }
+
+        [Fact]
+        public void Render_WithNullTexture_DoesNotRender()
+        {
+            // Arrange
+            var mockSpriteBatch = new Mock<ISpriteBatch>();
+
+            this.mockContentLoader.Setup(m => m.Load<ITexture>("button-face"))
+                .Returns(() =>
+                {
+                    ITexture? nullTexture = null;
+
+#pragma warning disable 8603
+                    return nullTexture;
+#pragma warning restore 8603
+                });
+            var button = CreateButton();
+            button.Visible = true;
+            button.LoadContent();
+
+            // Act
+            button.Render(mockSpriteBatch.Object);
+
+            // Assert
+            mockSpriteBatch.Verify(m =>
+                m.Render(It.IsAny<ITexture>(),
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<Color>()), Times.Never);
+        }
+
+        [Fact]
+        public void Render_WhenInvoked_RendersTexture()
+        {
+            // Arrange
+            var mockSpriteBatch = new Mock<ISpriteBatch>();
+            var button = CreateButton();
+            button.Position = new Point(10, 20);
+            button.Visible = true;
+            button.LoadContent();
+
+            // Act
+            button.Render(mockSpriteBatch.Object);
+
+            // Assert
+            mockSpriteBatch.Verify(m =>
+                m.Render(this.mockTexture.Object,
+                    110,
+                    70,
+                    Color.White), Times.Once);
+
+            this.mockLabel.Verify(m =>
+                m.Render(mockSpriteBatch.Object), Times.Once);
+        }
+
+        [Fact]
+        public void Dispose_WithNoLoadedContent_DoesNotThrowException()
+        {
+            // Arrange
+            var button = CreateButton();
+
+            // Act & Assert
+            AssertExtensions.DoesNotThrow<NullReferenceException>(() =>
+            {
+                button.Dispose();
+            });
+
+            this.mockTexture.Verify(m => m.Dispose(), Times.Never);
+        }
+
+        [Fact]
+        public void Dispose_WithLoadedContent_DisposesOfTexture()
+        {
+            // Arrange
+            var button = CreateButton();
+            button.LoadContent();
+
+            // Act & Assert
+            button.Dispose();
+            button.Dispose();
+
+            this.mockTexture.Verify(m => m.Dispose(), Times.Once);
+        }
+        #endregion
+
+        /// <summary>
+        /// Creates a new button for the purpose of testing.
+        /// </summary>
+        /// <returns>The button instance to test.</returns>
+        private Button CreateButton()
+            => new Button(this.mockContentLoader.Object, this.mockLabel.Object);
+    }
+}
--- Velaptor/UI/Button.cs
+++ Velaptor/UI/Button.cs
@@ -6,184 +6,114 @@ namespace Velaptor.UI
 {
     using System;
     using System.Drawing;
-    using System.Numerics;
     using Velaptor.Content;
     using Velaptor.Graphics;
 
     /// <summary>
-    /// A button that can be clicked and execute functionality.
+    /// A button that can be clicked to execute functionality.
     /// </summary>
-    public class Button : IControl
+    public sealed class Button : ControlBase
     {
-        private readonly bool isMouseDown;
-        private Rectangle rect;
+        private readonly IContentLoader contentLoader;
+        private readonly IControl label;
+        private ITexture? texture;
+        private bool isDisposed;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Button"/> class.
         /// </summary>
-        /// <param name="mouseOverTexture">The texture to be displayed when the mouse is over the button.</param>
-        /// <param name="mouseNotOverTexture">The texture to be displayed when the mouse is not in the down position or over the button.</param>
-        /// <param name="mouseDownTexture">The texture to be displayed when the mouse is in the down position.</param>
-        public Button(Texture mouseOverTexture, Texture mouseNotOverTexture, Texture mouseDownTexture)
+        /// <param name="contentLoader">Loads the button content for rendering.</param>
+        /// <param name="label">The label on the face of the button.</param>
+        public Button(IContentLoader contentLoader, IControl label)
         {
-            MouseOverTexture = mouseOverTexture;
-            MouseNotOverTexture = mouseNotOverTexture;
-            MouseDownTexture = mouseDownTexture;
+            this.contentLoader = contentLoader;
+            this.label = label;
         }
 
-        /// <summary>
-        /// Occurs when the button has been clicked.
-        /// </summary>
-        public event EventHandler<EventArgs>? Click;
+        /// <inheritdoc cref="IControl"/>
+        public override Point Position
+        {
+            get => base.Position;
+            set
+            {
+                base.Position = value;
 
-        /// <summary>
-        /// Gets or sets the position of the <see cref="Button"/> on the screen.
-        /// </summary>
-        public Vector2 Position { get; set; }
+                var posX = (int)(Position.X + ((Width / 2f) - (this.label.Width / 2f)));
+                var posY = (int)(Position.Y + ((Height / 2f) - (this.label.Height / 2f)));
+
+                this.label.Position = new Point(posX, posY);
+            }
+        }
 
         /// <summary>
         /// Gets the width of the <see cref="Button"/>.
         /// </summary>
-        public int Width
-        {
-            get
-            {
-                if (MouseOverTexture == null || MouseNotOverTexture == null)
-                {
-                    return 0;
-                }
-
-                return MouseOverTexture.Width > MouseNotOverTexture.Width ?
-                    MouseOverTexture.Width :
-                    MouseNotOverTexture.Width;
-            }
-        }
+        public override int Width => this.texture?.Width ?? 0;
 
         /// <summary>
         /// Gets the height of the <see cref="Button"/>.
         /// </summary>
-        public int Height
-        {
-            get
-            {
-                if (MouseOverTexture == null || MouseNotOverTexture == null)
-                {
-                    return 0;
-                }
-
-                return MouseOverTexture.Height > MouseNotOverTexture.Height ?
-                    MouseOverTexture.Height :
-                    MouseNotOverTexture.Height;
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the texture when the mouse is over the <see cref="Button"/>.
-        /// </summary>
-        public Texture MouseOverTexture { get; set; }
-
-        /// <summary>
-        /// Gets or sets the texture when the mouse is not over the <see cref="Button"/>.
-        /// </summary>
-        public Texture MouseNotOverTexture { get; set; }
-
-        /// <summary>
-        /// Gets or sets the texture when the left mouse button is
-        /// in the down position over the button.
-        /// </summary>
-        public Texture MouseDownTexture { get; set; }
-
-        /// <summary>
-        /// Gets a value indicating whether gets a value indicating if the mouse is hovering over the button.
-        /// </summary>
-        public bool IsMouseOver { get; private set; }
-
-        /// <summary>
-        /// Gets or sets the text of the button.
-        /// </summary>
-        public RenderText? ButtonText { get; set; }
-
-        /// <summary>
-        /// Initializes the <see cref="Button"/>.
-        /// </summary>
-        public void Initialize()
-        {
-        }
+        public override int Height => this.texture?.Height ?? 0;
 
         /// <summary>
         /// Loads the content for the <see cref="Button"/>.
         /// </summary>
-        /// <param name="contentLoader">The loader used to load the content.</param>
-        public void LoadContent(IContentLoader contentLoader)
+        public override void LoadContent()
         {
-        }
+            // TODO: Check if already loaded and return if it has been
+            // TODO: Add label with simple text.  This means adding a text property
+            this.texture = this.contentLoader.Load<ITexture>("button-face");
+            this.label.LoadContent();
 
-        /// <summary>
-        /// Updates the <see cref="Button"/>.
-        /// </summary>
-        /// <param name="engineTime">The update iteration time.</param>
-        public void Update(FrameTime engineTime)
-        {
-            ProcessMouse();
-
-            this.rect.X = (int)(Position.X - (Width / 2f));
-            this.rect.Y = (int)(Position.Y - (Height / 2f));
-            this.rect.Width = Width;
-            this.rect.Height = Height;
+            base.LoadContent();
         }
 
         /// <summary>
         /// Renders the <see cref="Button"/> to the screen.
         /// </summary>
-        /// <param name="renderer">Renders the <see cref="Button"/>.</param>
-        public void Render(object renderer)
+        /// <param name="spriteBatch">Renders the <see cref="Button"/>.</param>
+        public override void Render(ISpriteBatch spriteBatch)
         {
-            // if (renderer is null)
-            //    throw new ArgumentNullException(nameof(renderer), "The renderer must not be null.");
+            if (IsLoaded is false || Visible is false)
+            {
+                return;
+            }
 
-            // if (_isMouseDown && MouseDownTexture != null)
-            // {
-            //    renderer.Render(MouseDownTexture, Position.X, Position.Y);
-            // }
-            // else
-            // {
-            //    if (IsMouseOver && MouseOverTexture != null)
-            //    {
-            //        renderer.Render(MouseOverTexture, Position.X, Position.Y);
-            //    }
-            //    else
-            //    {
-            //        if (MouseNotOverTexture != null)
-            //            renderer.Render(MouseNotOverTexture, Position.X, Position.Y, 0);
-            //    }
+            if (this.texture is not null)
+            {
+                var posX = Position.X + (Width / 2);
+                var posY = Position.Y + (Height / 2);
 
-            // }
+                spriteBatch.Render(this.texture, posX, posY, TintColor);
 
-            // var textPosition = new Vector2()
-            // {
-            //    X = Position.X - (ButtonText is null ? 0 : ButtonText.Width / 2f),
-            //    Y = Position.Y - (ButtonText is null ? 0 :  ButtonText.Height / 2f)
-            // };
+                this.label.Render(spriteBatch);
+            }
 
-            // if(ButtonText != null)
-            //    renderer.Render(ButtonText, textPosition, Color.FromArgb(255, 0, 0, 0));
+            base.Render(spriteBatch);
         }
 
         /// <summary>
-        /// Processes any mouse input interaction with the <see cref="Button"/>.
+        /// <inheritdoc cref="IDisposable" />
         /// </summary>
-        private void ProcessMouse()
+        public override void Dispose() => Dispose(true);
+
+        /// <summary>
+        /// <inheritdoc cref="IDisposable"/>
+        /// </summary>
+        /// <param name="disposing">True to dispose of managed resources.</param>
+        private void Dispose(bool disposing)
         {
-            // this.mouse.UpdateCurrentState();
+            if (this.isDisposed)
+            {
+                return;
+            }
 
-            // IsMouseOver = this.rect.Contains(this.mouse.X, this.mouse.Y);
+            if (disposing)
+            {
+                this.texture?.Dispose();
+            }
 
-            // this.isMouseDown = IsMouseOver && this.mouse.IsButtonDown(MouseButton.LeftButton);
-
-            // if (this.isMouseDown)
-            //    Click?.Invoke(this, new EventArgs());
-
-            // this.mouse.UpdatePreviousState();
+            this.isDisposed = true;
         }
     }
 }
