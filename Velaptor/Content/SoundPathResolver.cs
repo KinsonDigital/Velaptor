134c2bb419e03c2a8babe419afdb0f1658b66a1e
Kristen Wilkinson <kswilky@gmail.com>
2022-01-02 10:46:18 -0500

    Removed "not" from after .mp3 file exists

--- Velaptor/Content/SoundPathResolver.cs
+++ Velaptor/Content/SoundPathResolver.cs
@@ -40,7 +40,7 @@ namespace Velaptor.Content
         ///     file will be loaded, not the '.mp3' file.
         /// </para>
         /// <para>
-        ///     If no '.ogg' file exists but a '.mp3' file does not, then the '.mp3' file will be loaded.
+        ///     If no '.ogg' file exists but a '.mp3' file does, then the '.mp3' file will be loaded.
         /// </para>
         /// </remarks>
         public override string ResolveFilePath(string contentName)
