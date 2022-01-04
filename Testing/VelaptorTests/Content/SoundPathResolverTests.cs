697922aac2deec31b0b88db69573011d2c767722
Kristen Wilkinson <kswilky@gmail.com>
2022-01-04 14:28:34 -0500

    Removed using statement alias

--- Testing/VelaptorTests/Content/SoundPathResolverTests.cs
+++ Testing/VelaptorTests/Content/SoundPathResolverTests.cs
@@ -9,8 +9,8 @@ namespace VelaptorTests.Content
     using System.Reflection;
     using Moq;
     using Velaptor.Content;
+    using VelaptorTests.Helpers;
     using Xunit;
-    using Assert = Helpers.AssertExtensions;
 
     /// <summary>
     /// Tests the <see cref="SoundPathResolver"/> class.
@@ -69,7 +69,7 @@ namespace VelaptorTests.Content
             var resolver = new SoundPathResolver(mockDirectory.Object);
 
             // Act & Assert
-            Assert.ThrowsWithMessage<FileNotFoundException>(() =>
+            AssertExtensions.ThrowsWithMessage<FileNotFoundException>(() =>
             {
                 resolver.ResolveFilePath(ContentName);
             }, $"The sound file '{this.contentFilePath}' does not exist.");
