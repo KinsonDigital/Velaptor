23c44f29955bb8fcb59bbcfcbf7cd8f25bbbeb5c
Calvin Wilkinson <kinsondigital@gmail.com>
2022-01-07 12:39:50 -0500

    🎉Create new exception that can be thrown when something goes wrong with caching

---
+++ Velaptor/Content/Exceptions/CachingException.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="CachingException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Content.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Thrown when there is an issue caching items.
+    /// </summary>
+    public class CachingException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachingException"/> class.
+        /// </summary>
+        public CachingException()
+            : base($"There was an issue caching the item.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachingException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public CachingException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CachingException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception,
+        ///     or a null reference if no inner exception is specified.
+        /// </param>
+        public CachingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
