fd8bd1a3f9ea1ba39e107049799460664aaa29aa
Calvin Wilkinson <kinsondigital@gmail.com>
2022-01-07 09:01:59 -0500

    🎉Add custom exception 'LoadAtlasException' that will be thrown when there is an issue loading atlas data

---
+++ Velaptor/Content/Exceptions/LoadAtlasException.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="LoadAtlasException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Content.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Thrown when there is an issue loading atlas data content.
+    /// </summary>
+    public class LoadAtlasException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadAtlasException"/> class.
+        /// </summary>
+        public LoadAtlasException()
+            : base($"There was an issue loading the atlas data.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadAtlasException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public LoadAtlasException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadAtlasException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">
+        ///     The exception that is the cause of the current exception,
+        ///     or a null reference if no inner exception is specified.
+        /// </param>
+        public LoadAtlasException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
