b1e521631050d10a3803ad2f43a76ee8c2bb79ff
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-03 16:34:07 +0100

    Code cleanup

--- Velaptor/Reactables/ShutDownReactable.cs
+++ Velaptor/Reactables/ShutDownReactable.cs
@@ -5,7 +5,6 @@
 namespace Velaptor.Reactables
 {
     // ReSharper disable RedundantNameQualifier
-    using System.Diagnostics.CodeAnalysis;
     using Velaptor.Reactables.Core;
     using Velaptor.Reactables.ReactableData;
 
@@ -21,10 +20,6 @@ namespace Velaptor.Reactables
         /// </summary>
         /// <param name="data">The data to send with the push notification.</param>
         /// <param name="unsubscribeAfterProcessing">If true, unsubscribes all of the reactors after the notification has been pushed.</param>
-        [SuppressMessage(
-            "ReSharper",
-            "ForCanBeConvertedToForeach",
-            Justification = "Required for proper reactable operation.")]
         public override void PushNotification(ShutDownData data, bool unsubscribeAfterProcessing = false)
         {
             /* Work from the end to the beginning of the list
