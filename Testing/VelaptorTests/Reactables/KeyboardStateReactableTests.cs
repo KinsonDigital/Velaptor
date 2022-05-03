1c89c4ef6f50baf1d56f0ab1551ba93f2962211e
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-03 12:56:45 +0100

    Create a reactable to send push notifications of the keyboard state

---
+++ Testing/VelaptorTests/Reactables/KeyboardStateReactableTests.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="KeyboardStateReactableTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.Reactables
+{
+    using Moq;
+    using Velaptor.Input;
+    using Velaptor.Reactables;
+    using Velaptor.Reactables.Core;
+    using Xunit;
+
+    public class KeyboardStateReactableTests
+    {
+        #region Method Tests
+        [Theory]
+        [InlineData(false, 1)]
+        [InlineData(true, 0)]
+        public void PushNotification_WhenInvoked_SendsPushNotification(bool unsubscribe, int expected)
+        {
+            // Arrange
+            var reactor = new Mock<IReactor<(KeyCode key, bool isDown)>>();
+
+            var reactable = new KeyboardStateReactable();
+            reactable.Subscribe(reactor.Object);
+
+            var data = (KeyCode.Space, true);
+
+            // Act
+            reactable.PushNotification(data, unsubscribe);
+
+            // Assert
+            reactor.Verify(m => m.OnNext(data), Times.Once());
+            Assert.Equal(expected, reactable.Reactors.Count);
+        }
+        #endregion
+    }
+}
