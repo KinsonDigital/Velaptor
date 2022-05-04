6765218849f846fc5a0acba2721af9e75d27b125
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-04 13:03:33 +0100

    🎉Create (or make public), a new factory for creating UI controls

---
+++ Velaptor/Factories/IUIControlFactory.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="IUIControlFactory.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Factories
+{
+    // ReSharper disable RedundantNameQualifier
+    using Velaptor.Content.Fonts;
+    using Velaptor.UI;
+
+    // ReSharper restore RedundantNameQualifier
+
+    /// <summary>
+    /// Creates UI controls.
+    /// </summary>
+    public interface IUIControlFactory
+    {
+        /// <summary>
+        /// Creates a new <see cref="Label"/> control to display text.
+        /// </summary>
+        /// <param name="labelText">The text to display in the label.</param>
+        /// <returns>The label to render.</returns>
+        Label CreateLabel(string labelText);
+
+        /// <summary>
+        /// Creates a new <see cref="Label"/> control to display text.
+        /// </summary>
+        /// <param name="labelText">The text to display in the label.</param>
+        /// <param name="font">The font to use for the label.</param>
+        /// <returns>The label to render.</returns>
+        Label CreateLabel(string labelText, IFont font);
+    }
+}
