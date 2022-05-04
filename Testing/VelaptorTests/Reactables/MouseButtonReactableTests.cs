5036671d54149f24ce9b0213b591ee82e6a9966b
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-04 13:01:49 +0100

    Create new reactable to update changes to the mouse buttons

---
+++ Testing/VelaptorTests/Reactables/MouseButtonReactableTests.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="MouseButtonReactableTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.Reactables
+{
+    using Moq;
+    using Velaptor.Input;
+    using Velaptor.Reactables;
+    using Velaptor.Reactables.Core;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="MouseButtonReactable"/> class.
+    /// </summary>
+    public class MouseButtonReactableTests
+    {
+        #region Method Tests
+        [Theory]
+        [InlineData(false, 1)]
+        [InlineData(true, 0)]
+        public void PushNotification_WhenInvoked_SendsPushNotification(bool unsubscribe, int expected)
+        {
+            // Arrange
+            var reactor = new Mock<IReactor<(MouseButton, bool)>>();
+
+            var reactable = new MouseButtonReactable();
+            reactable.Subscribe(reactor.Object);
+
+            var data = (MouseButton.LeftButton, true);
+
+            // Act
+            reactable.PushNotification(data, unsubscribe);
+
+            // Assert
+            reactor.Verify(m => m.OnNext(data), Times.Once());
+            Assert.Equal(expected, reactable.Reactors.Count);
+        }
+        #endregion
+    }
+}
