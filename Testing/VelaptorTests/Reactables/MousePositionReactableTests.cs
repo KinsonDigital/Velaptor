7048757632478623011d1a673e59fad984f52eb1
Calvin Wilkinson <kinsondigital@gmail.com>
2022-05-04 13:01:56 +0100

    Create new reactable to update changes to the position of the mouse

---
+++ Testing/VelaptorTests/Reactables/MousePositionReactableTests.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="MousePositionReactableTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.Reactables
+{
+    using Moq;
+    using Velaptor.Reactables;
+    using Velaptor.Reactables.Core;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="MousePositionReactable"/> class.
+    /// </summary>
+    public class MousePositionReactableTests
+    {
+        #region Method Tests
+        [Theory]
+        [InlineData(false, 1)]
+        [InlineData(true, 0)]
+        public void PushNotification_WhenInvoked_SendsPushNotification(bool unsubscribe, int expected)
+        {
+            // Arrange
+            var reactor = new Mock<IReactor<(int x, int y)>>();
+
+            var reactable = new MousePositionReactable();
+            reactable.Subscribe(reactor.Object);
+
+            var data = (11, 22);
+
+            // Act
+            reactable.PushNotification(data, unsubscribe);
+
+            // Assert
+            reactor.Verify(m => m.OnNext(data), Times.Once());
+            Assert.Equal(expected, reactable.Reactors.Count);
+        }
+        #endregion
+    }
+}
---
+++ Velaptor/Reactables/MousePositionReactable.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="MousePositionReactable.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Reactables
+{
+    // ReSharper disable RedundantNameQualifier
+    using Velaptor.Reactables.Core;
+
+    // ReSharper restore RedundantNameQualifier
+
+    /// <summary>
+    /// Creates a reactable to send push notifications to signal that the position of the mouse has changed.
+    /// </summary>
+    internal class MousePositionReactable : Reactable<(int x, int y)>
+    {
+        /// <summary>
+        /// Sends a push notification to signal mouse position changes.
+        /// </summary>
+        /// <param name="data">The data to send with the push notification.</param>
+        /// <param name="unsubscribeAfterProcessing">If true, unsubscribes all of the reactors after the notification has been pushed.</param>
+        public override void PushNotification((int x, int y) data, bool unsubscribeAfterProcessing = false)
+        {
+            /* Work from the end to the beginning of the list
+               just in case the reactable is disposed(removed)
+               in the OnNext() method.
+             */
+            for (var i = Reactors.Count - 1; i >= 0; i--)
+            {
+                Reactors[i].OnNext(data);
+            }
+
+            if (unsubscribeAfterProcessing)
+            {
+                UnsubscribeAll();
+            }
+        }
+    }
+}
