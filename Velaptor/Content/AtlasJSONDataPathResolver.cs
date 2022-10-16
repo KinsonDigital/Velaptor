19f97f8fe51491868f6a98bcfe9d815cfebccc02
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-16 23:35:54 +0100

    [bug] - Fix pathing bug in atlas JSON data path resolver

--- Velaptor/Content/AtlasJSONDataPathResolver.cs
+++ Velaptor/Content/AtlasJSONDataPathResolver.cs
@@ -49,8 +49,11 @@ namespace Velaptor.Content
 
             var contentDirPath = GetContentDirPath();
 
+            var possibleFiles = this.directory.GetFiles(contentDirPath, $"*{FileExtension}")
+                .NormalizePaths();
+
             // Check if there are any files that match the name
-            var files = (from f in this.directory.GetFiles(contentDirPath, $"*{FileExtension}")
+            var files = (from f in possibleFiles
                          where string.Compare(
                              f,
                              $"{contentDirPath}{CrossPlatDirSeparatorChar}{contentName}{FileExtension}",
