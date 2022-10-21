fbccff0ce8d1e0e68e85d658ce79d6b221a91ce6
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-21 16:43:44 +0100

    Create a new service to manage application settings

---
+++ Velaptor/Services/IAppSettingsService.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IAppSettingsService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Services;
+
+/// <summary>
+/// Provides management of application settings.
+/// </summary>
+internal interface IAppSettingsService
+{
+    /// <summary>
+    /// Gets the width of the game window.
+    /// </summary>
+    uint WindowWidth { get; }
+
+    /// <summary>
+    /// Gets the height of the game window.
+    /// </summary>
+    uint WindowHeight { get; }
+}
