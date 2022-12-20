30309b9e740dee7991d7da36ad30190781b5d497
Calvin Wilkinson <kinsondigital@gmail.com>
2022-12-20 18:21:05 +0000

    chore: create container to hold unique push notification IDs

---
+++ Velaptor/NotificationIds.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="NotificationIds.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor;
+
+using System;
+using Carbonate;
+
+/// <summary>
+/// Contains unique event GUIDs for the <see cref="Reactable"/> push notification system.
+/// </summary>
+internal static class NotificationIds
+{
+    /// <summary>
+    /// Gets the unique event <see cref="Guid"/> for push notifications for batch sizes.
+    /// </summary>
+    public static Guid BatchSizeId { get; } = new ("7819e8f0-9797-4bfd-b9ed-6505c8a6ca89");
+}
