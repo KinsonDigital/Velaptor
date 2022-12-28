c3815f8b003594b4746139058fe1576472c90c25
Kristen Wilkinson <kswilky@gmail.com>
2022-12-28 12:47:43 +0000

    Move internal visible setup

--- Velaptor/ExposedInternals.cs
+++ Velaptor/ExposedInternals.cs
@@ -1,10 +1,2 @@
-﻿// <copyright file="ExposedInternals.cs" company="KinsonDigital">
-// Copyright (c) KinsonDigital. All rights reserved.
-// </copyright>
+﻿
 
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo(assemblyName: "VelaptorTests", AllInternalsVisible = true)]
-
-// Used to expose internal types to the Moq mocking framework
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2", AllInternalsVisible = true)]
