155e7b310e7676ce7f43d995c632d25fc19db902
Calvin Wilkinson <kinsondigital@gmail.com>
2023-01-16 12:59:50 +0000

    chore: create new internal struct named 'RenderItem'

---
+++ Velaptor/OpenGL/Batching/RenderItem.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="RenderItem.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.OpenGL.Batching;
+
+using System;
+
+/// <summary>
+/// A single batch item with associated meta-data about how it should be rendered.
+/// </summary>
+/// <typeparam name="T">The <c>struct</c> that represents the batch item.</typeparam>
+internal readonly record struct RenderItem<T>
+    where T : struct
+{
+    /// <summary>
+    /// Gets the layer that the <see cref="BatchItem"/> should be rendered on.
+    /// </summary>
+    public int Layer { get; init; }
+
+    /// <summary>
+    /// Gets the date and time that the <see cref="BatchItem"/> was added for rendering.
+    /// </summary>
+    public DateTime DateTime { get; init; }
+
+    /// <summary>
+    /// Gets the batch item to render.
+    /// </summary>
+    public T BatchItem { get; init; }
+}
