e96b0d17500d8ab4693fb13c03f70ffcf794781b
Calvin Wilkinson <kinsondigital@gmail.com>
2023-01-25 11:23:38 +0000

    chore: create new reactable for the purpose of rendering

---
+++ Velaptor/Graphics/IRenderBatchReactable.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IRenderBatchReactable.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.Graphics;
+
+using System;
+using Carbonate.UniDirectional;
+using OpenGL.Batching;
+
+/// <summary>
+/// Sends batch data to a renderer for the purpose of rendering.
+/// </summary>
+/// <typeparam name="TBatchItem">The type of batch item to send.</typeparam>
+internal interface IRenderBatchReactable<TBatchItem> : IPushReactable<Memory<RenderItem<TBatchItem>>>
+{
+}
