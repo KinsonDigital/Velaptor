4224dabe744f09d33aa7147849455967da76c121
Calvin Wilkinson <kinsondigital@gmail.com>
2023-02-08 14:48:17 +0000

    chore: set the 'BatchSizeData' type to a record struct

---
+++ Testing/VelaptorTests/ReactableData/BatchSizeDataTests.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="BatchSizeDataTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.ReactableData;
+
+using FluentAssertions;
+using Velaptor;
+using Velaptor.ReactableData;
+using Xunit;
+
+/// <summary>
+/// Tests the <see cref="BatchSizeData"/> struct.
+/// </summary>
+public class BatchSizeDataTests
+{
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WhenInvoked_SetsPropsToCorrectValues()
+    {
+        // Arrange & Act
+        var sut = new BatchSizeData
+        {
+            BatchSize = 1,
+            TypeOfBatch = BatchType.Rect,
+        };
+
+        // Assert
+        sut.BatchSize.Should().Be(1);
+        sut.TypeOfBatch.Should().Be(BatchType.Rect);
+    }
+    #endregion
+}
