a088000c925cb9efceadb55e9c783f0dd9b759b1
Calvin Wilkinson <kinsondigital@gmail.com>
2023-02-08 14:58:57 +0000

    chore: change the 'ViewPortSizeData' to a readonly struct

---
+++ Testing/VelaptorTests/ReactableData/ViewPortSizeDataTests.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ViewPortSizeDataTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.ReactableData;
+
+using FluentAssertions;
+using Velaptor.ReactableData;
+using Xunit;
+
+/// <summary>
+/// Tests the <see cref="ViewPortSizeData"/> struct.
+/// </summary>
+public class ViewPortSizeDataTests
+{
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WhenInvoked_SetsPropValuesToCorrectResult()
+    {
+        // Arrange & Act
+        var sut = new ViewPortSizeData
+        {
+            Width = 1,
+            Height = 2,
+        };
+
+        // Assert
+        sut.Width.Should().Be(1);
+        sut.Height.Should().Be(2);
+    }
+    #endregion
+}
