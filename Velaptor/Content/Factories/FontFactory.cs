8779513643c4c82fd50f3a1fdf38663b79cf9a0e
Kristen Wilkinson <kswilky@gmail.com>
2023-02-23 13:48:22 +0000

    Fix spelling error

--- Velaptor/Content/Factories/FontFactory.cs
+++ Velaptor/Content/Factories/FontFactory.cs
@@ -24,7 +24,7 @@ internal sealed class FontFactory : IFontFactory
     /// <summary>
     /// Initializes a new instance of the <see cref="FontFactory"/> class.
     /// </summary>
-    /// <param name="fontService">Helper methods for <c>FreeType</c> like operations.</param>
+    /// <param name="fontService">Provides extensions/helpers to <c>FreeType</c> library functionality.</param>
     /// <param name="fontStatsService">Provides font stat services.</param>
     /// <param name="fontAtlasService">Provides services for building font atlas textures.</param>
     /// <param name="textureCache">Creates and caches textures for later retrieval.</param>
