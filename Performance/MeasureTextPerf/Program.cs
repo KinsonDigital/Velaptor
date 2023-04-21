1cd6283d5fb2c49ca4e0f31fd3a2b8cdd3589094
Calvin Wilkinson <kinsondigital@gmail.com>
2023-04-21 16:54:23 +0100

    chore: create perf project for measuring text

---
+++ Performance/MeasureTextPerf/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BenchmarkDotNet.Running;
+using MeasureTextPerf;
+
+#if DEBUG
+
+var benchMark = new Benchmarks
+{
+    TotalCharacters = 100,
+    TotalLines = 10
+};
+benchMark.IterationSetup();
+benchMark.MeasureString();
+
+#elif RELEASE
+
+var result = BenchmarkRunner.Run<Benchmarks>();
+
+Console.WriteLine(result);
+Console.ReadLine();
+
+#endif
