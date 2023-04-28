a44db27437d140ea5353c956594782a80cd998d9
Calvin Wilkinson <kinsondigital@gmail.com>
2023-04-28 17:45:55 +0100

    chore: reset textbox back to a blank slate

--- Velaptor/UI/TextBox.cs
+++ Velaptor/UI/TextBox.cs
@@ -4,65 +4,24 @@
 
 namespace Velaptor.UI;
 
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
 using Content;
-using Graphics.Renderers;
+using Content.Fonts;
+using Factories;
 
 /// <summary>
 /// Provides the ability to enter text into a box.
 /// </summary>
-[ExcludeFromCodeCoverage(Justification = "Waiting for implementation")]
-
-// NOTE: Left as internal to prevent library users from using the control until it is ready
-internal sealed class TextBox : ControlBase
+public sealed class TextBox : ControlBase
 {
-    private const int LEFTMARGIN = 5;
-    private const int RIGHTMARGIN = 5;
-    private readonly int visibleTextCharPosition;
-    private readonly int charPosDelta;
-    private readonly int characterPosition;
-    private readonly int lastDirectionOfTravel;
-    private Texture backgroundTexture;
-    private int rightSide;
-    private int leftSide;
-    private bool cursorVisible;
-    private int cursorElapsedMilliseconds;
-    private Label text;
-    private readonly IContentLoader contentLoader;
+    private readonly ILoader<IFont> contentLoader;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="TextBox"/> class.
     /// </summary>
-    /// <param name="contentLoader">Loads various kinds of content.</param>
-    public TextBox(IContentLoader contentLoader)
+    public TextBox()
     {
-        this.contentLoader = contentLoader;
-
-        this.text = new Label(contentLoader, null, IoC.Container.GetInstance<IRendererFactory>())
-        {
-            Position = Point.Empty,
-            Text = "Textbox Text",
-        };
-
-        // Set position with left and to margin taken into account
+        this.contentLoader = ContentLoaderFactory.CreateFontLoader();
     }
-
-    /// <summary>
-    /// Gets the width of the <see cref="TextBox"/>.
-    /// </summary>
-    public override uint Width => this.backgroundTexture.Width;
-
-    /// <summary>
-    /// Gets the height of the <see cref="TextBox"/>.
-    /// </summary>
-    public override uint Height => this.backgroundTexture.Height;
-
-    /// <summary>
-    /// Gets or sets the font name of the <see cref="TextBox"/>.
-    /// </summary>
-    public string FontName { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the text in the <see cref="TextBox"/>.
@@ -72,7 +31,9 @@ internal sealed class TextBox : ControlBase
     /// <summary>
     /// Loads the content of the <see cref="TextBox"/>.
     /// </summary>
-    public override void LoadContent() => this.text.LoadContent();
+    public override void LoadContent()
+    {
+    }
 
     /// <summary>
     /// Updates the text box.
@@ -84,18 +45,6 @@ internal sealed class TextBox : ControlBase
         {
             return;
         }
-
-        this.text.Update(frameTime);
-
-        ProcessKeys();
-
-        this.cursorElapsedMilliseconds += frameTime.ElapsedTime.Milliseconds;
-
-        if (this.cursorElapsedMilliseconds >= 500)
-        {
-            this.cursorElapsedMilliseconds = 0;
-            this.cursorVisible = !this.cursorVisible;
-        }
     }
 
     /// <inheritdoc/>
@@ -106,7 +55,6 @@ internal sealed class TextBox : ControlBase
             return;
         }
 
-        // this.text.Render(renderer);
     }
 
     /// <summary>
@@ -115,14 +63,4 @@ internal sealed class TextBox : ControlBase
     private static void ProcessKeys()
     {
     }
-
-    /// <summary>
-    /// Removes the characters using the backspace key.
-    /// </summary>
-    private static void RemoveCharacterUsingBackspace()
-    {
-        // Text = Text.Remove(Text.IndexOf(this.visibleText.Text, StringComparison.Ordinal) + this.characterPosition - 1, 1);
-    }
-
-    public void Dispose() => throw new NotImplementedException();
 }
