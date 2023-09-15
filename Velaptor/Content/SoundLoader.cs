6531293f7cfd00999c91c399ba37f00ed00f75e1
Calvin Wilkinson <kinsondigital@gmail.com>
2023-09-15 15:07:52 +0100

    fix: fix issue with sound and texture loader

--- Velaptor/Content/SoundLoader.cs
+++ Velaptor/Content/SoundLoader.cs
@@ -97,7 +97,7 @@ public sealed class SoundLoader : ILoader<ISound>
         {
             var contentDirPath = this.soundPathResolver.ResolveDirPath();
 
-            if (this.directory.Exists(contentDirPath))
+            if (!this.directory.Exists(contentDirPath))
             {
                 this.directory.CreateDirectory(contentDirPath);
             }
