8d8f94bb96f871136a1b71097ed107fc93348627
Mario Andrés Pérez Batanero <mapb_@outlook.com>
2023-11-17 00:47:25 +0100

    Add and use Constructor with positional parameters to record struct.

--- Velaptor/Graphics/FontAtlasMetrics.cs
+++ Velaptor/Graphics/FontAtlasMetrics.cs
@@ -9,26 +9,30 @@ using System.Diagnostics.CodeAnalysis;
 
 /// <summary>
 /// Provides metric data for a single font atlas texture.
+/// <param name="Rows">Number of rows in the atlas.</param>
+/// <param name="Columns">Number of columns in the atlas.</param>
+/// <param name="Width">Width of the atlas.</param>
+/// <param name="Height">Height of the atlas.</param>
 /// </summary>
-internal readonly record struct FontAtlasMetrics
+internal readonly record struct FontAtlasMetrics(uint Rows, uint Columns, uint Width, uint Height)
 {
     /// <summary>
     /// Gets the number of rows in the atlas.
     /// </summary>
-    public uint Rows { get; init; }
+    public uint Rows { get; init; } = Rows;
 
     /// <summary>
     /// Gets the number of columns in the atlas.
     /// </summary>
-    public uint Columns { get; init; }
+    public uint Columns { get; init; } = Columns;
 
     /// <summary>
     /// Gets the width of the atlas.
     /// </summary>
-    public uint Width { get; init; }
+    public uint Width { get; init; } = Width;
 
     /// <summary>
     /// Gets the height of the atlas.
     /// </summary>
-    public uint Height { get; init; }
+    public uint Height { get; init; } = Height;
 }
--- Velaptor/Services/FontAtlasService.cs
+++ Velaptor/Services/FontAtlasService.cs
@@ -129,7 +129,7 @@ internal sealed class FontAtlasService : IFontAtlasService
         var width = maxGlyphWidth * columns;
         var height = maxGlyphHeight * rows;
 
-        return new FontAtlasMetrics { Rows = rows, Columns = columns, Width = width, Height = height };
+        return new FontAtlasMetrics(rows, columns, width, height);
     }
 
     /// <summary>
