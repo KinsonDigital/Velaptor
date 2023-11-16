54333bb8dc09ed9d4a16328cbbf106e1b0dcf80f
Mario Andrés Pérez Batanero <mapb_@outlook.com>
2023-11-17 00:47:25 +0100

    Add and use Constructor with positional parameters to record struct.

--- Velaptor/Graphics/FontAtlasMetrics.cs
+++ Velaptor/Graphics/FontAtlasMetrics.cs
@@ -4,9 +4,6 @@
 
 namespace Velaptor.Graphics;
 
-using System;
-using System.Diagnostics.CodeAnalysis;
-
 /// <summary>
 /// Provides metric data for a single font atlas texture.
 /// <param name="Rows">Number of rows in the atlas.</param>
@@ -14,25 +11,4 @@ using System.Diagnostics.CodeAnalysis;
 /// <param name="Width">Width of the atlas.</param>
 /// <param name="Height">Height of the atlas.</param>
 /// </summary>
-internal readonly record struct FontAtlasMetrics(uint Rows, uint Columns, uint Width, uint Height)
-{
-    /// <summary>
-    /// Gets the number of rows in the atlas.
-    /// </summary>
-    public uint Rows { get; init; } = Rows;
-
-    /// <summary>
-    /// Gets the number of columns in the atlas.
-    /// </summary>
-    public uint Columns { get; init; } = Columns;
-
-    /// <summary>
-    /// Gets the width of the atlas.
-    /// </summary>
-    public uint Width { get; init; } = Width;
-
-    /// <summary>
-    /// Gets the height of the atlas.
-    /// </summary>
-    public uint Height { get; init; } = Height;
-}
+internal readonly record struct FontAtlasMetrics(uint Rows, uint Columns, uint Width, uint Height);
