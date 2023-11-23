e336665b07a5b5b36bb979b6b62afbadf3718a15
Mario Andrés Pérez Batanero <mapb_@outlook.com>
2023-11-23 23:27:22 +0100

    Refactor: GlyphMetrics struct to readonly record struct.

--- Velaptor/Graphics/GlyphMetrics.cs
+++ Velaptor/Graphics/GlyphMetrics.cs
@@ -11,21 +11,33 @@ using System.Drawing;
 /// <summary>
 /// Holds all of the various metrics of a glyph for rendering purposes.
 /// </summary>
-public struct GlyphMetrics : IEquatable<GlyphMetrics>
+public readonly record struct GlyphMetrics(char Glyph,
+                                           RectangleF GlyphBounds,
+                                           float Ascender,
+                                           float Descender,
+                                           float HoriBearingX,
+                                           float HoriBearingY,
+                                           float GlyphWidth,
+                                           float GlyphHeight,
+                                           float XMin,
+                                           float XMax,
+                                           float YMin,
+                                           float YMax,
+                                           uint CharIndex)
 {
     /// <summary>
-    /// Gets or sets the glyph character.
+    /// Gets the glyph character.
     /// </summary>
-    public char Glyph { get; set; }
+    public char Glyph { get; init; }
 
     /// <summary>
-    /// Gets or sets the rectangular bounds of where in a font texture
+    /// Gets the rectangular bounds of where in a font texture
     /// atlas the given <see cref="Glyph"/> resides.
     /// </summary>
-    public RectangleF GlyphBounds { get; set; }
+    public RectangleF GlyphBounds { get; init; }
 
     /// <summary>
-    /// Gets or sets the vertical distance from the horizontal baseline to the highest 'character'
+    /// Gets the vertical distance from the horizontal baseline to the highest 'character'
     /// coordinate in a font face.
     /// </summary>
     /// <remarks>
@@ -34,10 +46,10 @@ public struct GlyphMetrics : IEquatable<GlyphMetrics>
     ///     (without accents). For others it is the ascent of the highest accented character. Finally,
     ///     other formats define it as being equal to Y max value of the global bounding box.
     /// </remarks>
-    public float Ascender { get; set; }
+    public float Ascender { get; init; }
 
     /// <summary>
-    /// Gets or sets the vertical distance from the horizontal baseline to the lowest ‘character’ coordinate in a font face.
+    /// Gets the vertical distance from the horizontal baseline to the lowest ‘character’ coordinate in a font face.
     /// </summary>
     /// <remarks>
     ///     Unfortunately, font formats don't define the descender in a uniform way. For some formats,
@@ -45,64 +57,65 @@ public struct GlyphMetrics : IEquatable<GlyphMetrics>
     ///     the ascent of the lowest accented character. Finally, other formats define it as being equal
     ///     to the Y min value of the global bounding box. This field is negative for values below the baseline.
     /// </remarks>
-    public float Descender { get; set; }
+    public float Descender { get; init; }
 
     /// <summary>
-    /// Gets or sets the horizontal distance from the current cursor position to
+    /// Gets the horizontal distance from the current cursor position to
     /// the leftmost border of the glyph image's bounding box.
     /// </summary>
-    public float HoriBearingX { get; set; }
+    public float HoriBearingX { get; init; }
 
     /// <summary>
-    /// Gets or sets the vertical distance from the current cursor position
+    /// Gets the vertical distance from the current cursor position
     /// (on the baseline) to the top most border of the glyph image's bounding box.
     /// </summary>
-    public float HoriBearingY { get; set; }
+    public float HoriBearingY { get; init; }
 
     /// <summary>
-    /// Gets or sets the horizontal distance to increment the pen position when the glyph
+    /// Gets the horizontal distance to increment the pen position when the glyph
     /// is drawn as part of a string of text.
     /// </summary>
-    public float HorizontalAdvance { get; set; }
+    public float HorizontalAdvance { get; init; }
 
     /// <summary>
-    /// Gets or sets the glyph's width.
+    /// Gets the glyph's width.
     /// </summary>
-    public float GlyphWidth { get; set; }
+    public float GlyphWidth { get; init; }
 
     /// <summary>
-    /// Gets or sets the glyph's height.
+    /// Gets the glyph's height.
     /// </summary>
-    public float GlyphHeight { get; set; }
+    public float GlyphHeight { get; init; }
 
     /// <summary>
-    /// Gets or sets the horizontal minimum (left-most).
+    /// Gets the horizontal minimum (left-most).
     /// </summary>
-    public float XMin { get; set; }
+    public float XMin { get; init; }
 
     /// <summary>
-    /// Gets or sets the horizontal maximum (right-most).
+    /// Gets the horizontal maximum (right-most).
     /// </summary>
-    public float XMax { get; set; }
+    public float XMax { get; init; }
 
     /// <summary>
-    /// Gets or sets the vertical minimum (bottom-most).
+    /// Gets the vertical minimum (bottom-most).
     /// </summary>
-    public float YMin { get; set; }
+    public float YMin { get; init; }
 
     /// <summary>
-    /// Gets or sets the vertical maximum (top-most).
+    /// Gets the vertical maximum (top-most).
     /// </summary>
-    public float YMax { get; set; }
+    public float YMax { get; init; }
 
     /// <summary>
-    /// Gets or sets the glyph index.
+    /// Gets the glyph index.
     /// </summary>
     /// <remarks>
     ///     The value of 0 means ‘undefined character code’.
     /// </remarks>
-    public uint CharIndex { get; set; }
+    public uint CharIndex { get; init; }
 
+    /*
     /// <summary>
     /// Returns a value indicating whether or not the <paramref name="left"/> operand is equal to the <paramref name="right"/> operand.
     /// </summary>
@@ -164,4 +177,5 @@ public struct GlyphMetrics : IEquatable<GlyphMetrics>
 
         return hashCode.ToHashCode();
     }
+    */
 }
