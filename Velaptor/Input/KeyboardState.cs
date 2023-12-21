749576eb7e0c919aae097ed778c246d450bb4f6f
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-21 13:27:06 +0000

    refactor: improve keyboard state by using the keyboard key groups

--- Velaptor/Input/KeyboardState.cs
+++ Velaptor/Input/KeyboardState.cs
@@ -14,86 +14,6 @@ using System.Linq;
 /// </summary>
 public struct KeyboardState : IEquatable<KeyboardState>
 {
-    /// <summary>
-    /// The letter keys including the space bar.
-    /// </summary>
-    private static readonly KeyCode[] LetterKeys =
-    {
-        KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E,
-        KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J,
-        KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O,
-        KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T,
-        KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y,
-        KeyCode.Z, KeyCode.Space,
-    };
-
-    /// <summary>
-    /// The standard number keys above the letter keys.
-    /// </summary>
-    private static readonly KeyCode[] StandardNumberKeys =
-    {
-        KeyCode.D0, KeyCode.D1, KeyCode.D2,
-        KeyCode.D3, KeyCode.D4, KeyCode.D5,
-        KeyCode.D6, KeyCode.D7, KeyCode.D8,
-        KeyCode.D9,
-    };
-
-    /// <summary>
-    /// The number keys on the numpad.
-    /// </summary>
-    private static readonly KeyCode[] NumpadNumberKeys =
-    {
-        KeyCode.KeyPad0, KeyCode.KeyPad1, KeyCode.KeyPad2,
-        KeyCode.KeyPad3, KeyCode.KeyPad4, KeyCode.KeyPad5,
-        KeyCode.KeyPad6, KeyCode.KeyPad7, KeyCode.KeyPad8,
-        KeyCode.KeyPad9,
-    };
-
-    /// <summary>
-    /// The symbol keys.
-    /// </summary>
-    private static readonly KeyCode[] SymbolKeys =
-    {
-        KeyCode.Semicolon, KeyCode.Equal, KeyCode.Comma, KeyCode.Minus, KeyCode.Period, KeyCode.Slash,
-        KeyCode.LeftBracket, KeyCode.RightBracket, KeyCode.Apostrophe, KeyCode.KeyPadDivide, KeyCode.KeyPadMultiply,
-        KeyCode.KeyPadSubtract, KeyCode.KeyPadAdd, KeyCode.KeyPadDecimal, KeyCode.Backslash,
-    };
-
-    /// <summary>
-    /// The characters produced by the standard number keys when no shift modifier keys are in the down position.
-    /// </summary>
-    private static readonly Dictionary<KeyCode, char> NoShiftStandardNumberCharacters = new ()
-    {
-        { KeyCode.D0, '0' }, { KeyCode.D1, '1' }, { KeyCode.D2, '2' },
-        { KeyCode.D3, '3' }, { KeyCode.D4, '4' }, { KeyCode.D5, '5' },
-        { KeyCode.D6, '6' }, { KeyCode.D7, '7' }, { KeyCode.D8, '8' }, { KeyCode.D9, '9' },
-    };
-
-    /// <summary>
-    /// The symbol keys produced by the keyboard when no shift modifier keys are in the down position.
-    /// </summary>
-    private static readonly Dictionary<KeyCode, char> NoShiftSymbolCharacters = new ()
-    {
-        { KeyCode.Equal, '=' }, { KeyCode.Comma, ',' }, { KeyCode.Minus, '-' }, { KeyCode.Period, '.' }, { KeyCode.Slash, '/' },
-        { KeyCode.Backslash, '\\' }, { KeyCode.LeftBracket, '[' }, { KeyCode.RightBracket, ']' },
-        { KeyCode.Apostrophe, '\'' }, { KeyCode.Semicolon, ';' }, { KeyCode.KeyPadAdd, '+' }, { KeyCode.KeyPadDecimal, '.' },
-        { KeyCode.KeyPadDivide, '/' }, { KeyCode.KeyPadMultiply, '*' }, { KeyCode.KeyPadSubtract, '-' },
-    };
-
-    /// <summary>
-    /// The symbol keys produced by the keyboard when any shift modifier keys are in the down position.
-    /// </summary>
-    private static readonly Dictionary<KeyCode, char> WithShiftSymbolCharacters = new ()
-    {
-        { KeyCode.Equal, '+' }, { KeyCode.Comma, '<' }, { KeyCode.Minus, '_' }, { KeyCode.Period, '>' }, { KeyCode.Slash, '?' },
-        { KeyCode.Backslash, '|' }, { KeyCode.LeftBracket, '{' }, { KeyCode.RightBracket, '}' },
-        { KeyCode.Apostrophe, '\'' }, { KeyCode.Semicolon, ':' }, { KeyCode.D1, '!' },
-        { KeyCode.D2, '@' }, { KeyCode.D3, '#' }, { KeyCode.D4, '$' }, { KeyCode.D5, '%' },
-        { KeyCode.D6, '^' }, { KeyCode.D7, '&' }, { KeyCode.D8, '*' }, { KeyCode.D9, '(' },
-        { KeyCode.D0, ')' }, { KeyCode.KeyPadDivide, '/' }, { KeyCode.KeyPadMultiply, '*' }, { KeyCode.KeyPadSubtract, '-' },
-        { KeyCode.KeyPadAdd, '+' }, { KeyCode.KeyPadDecimal, '.' },
-    };
-
     private Dictionary<KeyCode, bool> keyStates;
 
     /*
@@ -260,7 +180,7 @@ public struct KeyboardState : IEquatable<KeyboardState>
         InitKeys();
 
         // Check all of the standard number keys
-        foreach (var key in StandardNumberKeys)
+        foreach (var key in KeyboardKeyGroups.StandardNumberKeys)
         {
             if (IsKeyDown(key))
             {
@@ -279,7 +199,7 @@ public struct KeyboardState : IEquatable<KeyboardState>
     public bool AnyNumpadNumberKeysDown()
     {
         // Check all of the numpad number keys
-        foreach (var key in NumpadNumberKeys)
+        foreach (var key in KeyboardKeyGroups.NumpadNumberKeys)
         {
             if (IsKeyDown(key))
             {
@@ -319,13 +239,13 @@ public struct KeyboardState : IEquatable<KeyboardState>
     {
         if (AnyShiftKeysDown())
         {
-            if (LetterKeys.Contains(key))
+            if (KeyboardKeyGroups.LetterKeys.Contains(key))
             {
                 return key == KeyCode.Space ? ' ' : key.ToString().ToUpperInvariant()[0];
             }
 
             // When the shift key is down, the standard number keys and symbol keys return symbols.
-            if (StandardNumberKeys.Contains(key))
+            if (KeyboardKeyGroups.StandardNumberKeys.Contains(key))
             {
                 return KeyboardKeyGroups.WithShiftSymbolCharacters[key];
             }
@@ -333,7 +253,7 @@ public struct KeyboardState : IEquatable<KeyboardState>
             return KeyboardKeyGroups.WithShiftSymbolCharacters[key];
         }
 
-        if (LetterKeys.Contains(key))
+        if (KeyboardKeyGroups.LetterKeys.Contains(key))
         {
 #pragma warning disable CA1304 // Specify CultureInfo
             return key == KeyCode.Space ? ' ' : key.ToString().ToLower()[0];
@@ -346,9 +266,9 @@ public struct KeyboardState : IEquatable<KeyboardState>
         }
 
         // When the shift is up, the standard number keys return numbers.
-        if (StandardNumberKeys.Contains(key))
+        if (KeyboardKeyGroups.StandardNumberKeys.Contains(key))
         {
-            return NoShiftStandardNumberCharacters[key];
+            return KeyboardKeyGroups.NoShiftStandardNumberCharacters[key];
         }
 
         return (char)0;
