3ae49d19939c402d4195e67002381d17adf092bc
Calvin Wilkinson <kinsondigital@gmail.com>
2024-01-10 22:51:46 +0000

    feat: create service to simplify and extend imgui related operations

---
+++ Velaptor/NativeInterop/Services/IImGuiService.cs
@@ -0,0 +1,41 @@
+// <copyright file="IImGuiService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor.NativeInterop.Services;
+
+/// <summary>
+/// Provides extra ImGui functionality.
+/// </summary>
+internal interface IImGuiService
+{
+    /// <summary>
+    /// Gets the pixel data, texture width, and texture height of the font atlas.
+    /// </summary>
+    /// <returns>The atlas related data.</returns>
+    // ReSharper disable once InconsistentNaming
+    (byte[] bytes, int textureWidth, int textureHeight) GetTexDataAsRGBA32();
+
+    /// <summary>
+    /// Sets the ID of the texture atlas of the font.
+    /// </summary>
+    /// <param name="textureId">The texture id.</param>
+    // ReSharper disable once InconsistentNaming
+    void SetTexID(uint textureId);
+
+    /// <summary>
+    /// Clears all of the currently loaded <see cref="ImGui"/> fonts.
+    /// </summary>
+    void ClearFonts();
+
+    /// <summary>
+    /// Clears the font texture atlas data from memory.
+    /// </summary>
+    void ClearTexData();
+
+    /// <summary>
+    /// Adds an embedded font to <see cref="ImGui"/> wiht the given <paramref name="fontSize"/>.
+    /// </summary>
+    /// <param name="fontSize">The size of the font.</param>
+    void AddEmbeddedFont(uint fontSize);
+}
