5b03b6443a38b7421bb2f6d2e13d68bbefcdd692
Calvin Wilkinson <kinsondigital@gmail.com>
2024-01-31 22:24:25 +0000

    fix: create notification ids for pulling data

---
+++ Velaptor/PullNotifications.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PullNotifications.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace Velaptor;
+
+using System;
+
+/// <summary>
+/// Contains unique GUIDs for the pull notification system.
+/// </summary>
+internal static class PullNotifications
+{
+    /// <summary>
+    /// Gets the unique <see cref="Guid"/> for pull notifications of when requesting the size of the window.
+    /// </summary>
+    public static Guid GetWindowSizeId { get; } = new ("76bef3ba-a4c8-49c8-9842-9fa4e3bd7692");
+}
