a57aa9176cb87b938f790efed453655e5051dd2b
Andrea Bondanini <bondaniniandrea@gmail.com>
2024-03-03 17:31:46 +0100

    Add DotnetService

---
+++ Velaptor/Services/DotnetService.cs
@@ -0,0 +1,19 @@
+namespace Velaptor.Services;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+/// <inheritdoc />
+[ExcludeFromCodeCoverage(Justification = $"Cannot test due to direct interaction with the dotnet {nameof(System)}.{nameof(GC)} and {nameof(System)}.{nameof(System.Runtime)}.{nameof(System.Runtime.InteropServices)}.{nameof(Marshal)} API.")]
+internal sealed class DotnetService : IDotnetService
+{
+    /// <inheritdoc />
+    public void GCKeepAlive(object? obj) => GC.KeepAlive(obj);
+
+    /// <inheritdoc />
+    public nint MarshalStringToHGlobalAnsi(string? s) => Marshal.StringToHGlobalAnsi(s);
+
+    /// <inheritdoc />
+    public string? MarshalPtrToStringAnsi(nint ptr) => Marshal.PtrToStringAnsi(ptr);
+}
