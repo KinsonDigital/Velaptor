48363c3a7a2e8f2a7a685110cce688712a39bfcd
CalvinWilkinson <kinsondigital@gmail.com>
2025-11-08 11:43:19 +0000

    test: exclude various areas from test code coverage reports

--- Velaptor/Services/IStopWatchWrapper.cs
+++ Velaptor/Services/IStopWatchWrapper.cs
@@ -5,11 +5,13 @@
 namespace Velaptor.Services;
 
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 /// <inheritdoc cref="Stopwatch"/>
 internal interface IStopWatchWrapper
 {
     /// <inheritdoc cref="Stopwatch.Frequency"/>
+    [ExcludeFromCodeCoverage(Justification = "Cannot test stopwatch property.")]
     long Frequency => Stopwatch.Frequency;
 
     /// <inheritdoc cref="Stopwatch.GetTimestamp"/>
