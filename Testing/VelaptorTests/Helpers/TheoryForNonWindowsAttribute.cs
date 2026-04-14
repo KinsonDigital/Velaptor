b43aba18c09609fb75343b5d5aba2aba52134be2
CalvinWilkinson <kinsondigital@gmail.com>
2026-04-14 17:49:57 +0100

    test: create test helper and os specific tests

---
+++ Testing/VelaptorTests/Helpers/TheoryForNonWindowsAttribute.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="TheoryForNonWindowsAttribute.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace VelaptorTests.Helpers;
+
+using System.Runtime.InteropServices;
+using Xunit;
+
+/// <summary>
+/// <inheritdoc cref="TheoryAttribute"/>.
+/// <para>
+///     Test is only executed for Non-<c>Windows</c> platform.
+/// </para>
+/// </summary>
+public sealed class TheoryForNonWindowsAttribute : TheoryAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TheoryForNonWindowsAttribute"/> class.
+    /// </summary>
+    public TheoryForNonWindowsAttribute()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return;
+        }
+
+        Skip = $"Only executed on Non-{OSPlatform.Windows}.";
+    }
+}
