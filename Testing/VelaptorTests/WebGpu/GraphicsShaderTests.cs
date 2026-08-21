0dc417ccdf669b5d6db325f0c4457d312437337b
Calvin Wilkinson <calvin@kinsondigital.net>
2026-08-21 15:33:21 +0100

    test: improve & and tests to graphics shader

--- Testing/VelaptorTests/WebGpu/GraphicsShaderTests.cs
+++ Testing/VelaptorTests/WebGpu/GraphicsShaderTests.cs
@@ -19,35 +19,33 @@ using Xunit;
 /// </summary>
 public class GraphicsShaderTests
 {
-    private const string ShaderName = "testShader";
     private const nint UnsafeVertexHandle = 0x1111;
     private const nint UnsafeFragmentHandle = 0x2222;
     private readonly SafeShaderModuleHandle vertexHandle;
     private readonly SafeShaderModuleHandle fragmentHandle;
-    private readonly IWgpuInvoker mockWgpuInvoker;
     private readonly IEmbeddedResourceLoaderService<string> mockResourceLoader;
     private readonly IPath mockPath;
-    private readonly IGraphicsDevice mockDevice;
+    private readonly IGraphicsDevice mockGrfxDevice;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="GraphicsShaderTests"/> class.
     /// </summary>
     public GraphicsShaderTests()
     {
-        this.mockWgpuInvoker = Substitute.For<IWgpuInvoker>();
+        var mockWgpuInvoker = Substitute.For<IWgpuInvoker>();
 
-        this.vertexHandle = new SafeShaderModuleHandle(this.mockWgpuInvoker, UnsafeVertexHandle);
-        this.fragmentHandle = new SafeShaderModuleHandle(this.mockWgpuInvoker, UnsafeFragmentHandle);
+        this.vertexHandle = new SafeShaderModuleHandle(mockWgpuInvoker, UnsafeVertexHandle);
+        this.fragmentHandle = new SafeShaderModuleHandle(mockWgpuInvoker, UnsafeFragmentHandle);
 
         this.mockResourceLoader = Substitute.For<IEmbeddedResourceLoaderService<string>>();
-        this.mockResourceLoader.LoadResource($"{ShaderName}.vert.wgsl").Returns("vert source");
-        this.mockResourceLoader.LoadResource($"{ShaderName}.frag.wgsl").Returns("frag source");
+        this.mockResourceLoader.LoadResource("Texture.vert.wgsl").Returns("vert source");
+        this.mockResourceLoader.LoadResource("Texture.frag.wgsl").Returns("frag source");
 
         this.mockPath = Substitute.For<IPath>();
 
-        this.mockDevice = Substitute.For<IGraphicsDevice>();
-        this.mockDevice.CreateShaderModule("vert source").Returns(this.vertexHandle);
-        this.mockDevice.CreateShaderModule("frag source").Returns(this.fragmentHandle);
+        this.mockGrfxDevice = Substitute.For<IGraphicsDevice>();
+        this.mockGrfxDevice.CreateShaderModule("vert source").Returns(this.vertexHandle);
+        this.mockGrfxDevice.CreateShaderModule("frag source").Returns(this.fragmentHandle);
     }
 
     #region Ctor Tests
@@ -55,7 +53,7 @@ public class GraphicsShaderTests
     public void Ctor_WithNullResourceLoaderServiceParam_ThrowsException()
     {
         // Arrange & Act
-        var act = () => { _ = new GraphicsShader(null, this.mockPath, ShaderName); };
+        var act = () => { _ = new GraphicsShader(null, this.mockPath); };
 
         // Assert
         act.ShouldThrow<ArgumentNullException>().Message.ShouldBe("Value cannot be null. (Parameter 'resourceLoaderService')");
@@ -65,89 +63,55 @@ public class GraphicsShaderTests
     public void Ctor_WithNullPathParam_ThrowsException()
     {
         // Arrange & Act
-        var act = () => { _ = new GraphicsShader(this.mockResourceLoader, null, ShaderName); };
+        var act = () => { _ = new GraphicsShader(this.mockResourceLoader, null); };
 
         // Assert
         act.ShouldThrow<ArgumentNullException>().Message.ShouldBe("Value cannot be null. (Parameter 'path')");
     }
     #endregion
 
-    #region Prop Tests
-    [Fact]
-    public void VertexHandle_WhenNotInitialized_ThrowsException()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        var act = () => _ = sut.VertexHandle;
-        // Assert
-        act.ShouldThrow<InvalidOperationException>()
-            .Message.ShouldBe("Shader has not been initialized. Call Initialize() first.");
-    }
-
-    [Fact]
-    public void VertexHandle_WhenInitialized_ReturnsHandle()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        sut.Initialize(this.mockDevice);
-        var actual = sut.VertexHandle;
-
-        // Assert
-        actual.ShouldBe(this.vertexHandle);
-    }
-
-    [Fact]
-    public void FragmentHandle_WhenNotInitialized_ThrowsException()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        var act = () => _ = sut.FragmentHandle;
-
-        // Assert
-        act.ShouldThrow<InvalidOperationException>()
-            .Message.ShouldBe("Shader has not been initialized. Call Initialize() first.");
-    }
-
-    [Fact]
-    public void FragmentHandle_WhenInitialized_ReturnsHandle()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        sut.Initialize(this.mockDevice);
-        var actual = sut.FragmentHandle;
-
-        // Assert
-        actual.ShouldBe(this.fragmentHandle);
-    }
-    #endregion
-
     #region Method Tests
+    [Fact]
+    public void Initialize_WithInvalidShaderType_ThrowsException()
+    {
+        // Arrange
+        var sut = CreateSystemUnderTest();
+
+        // Act
+        var act = () => sut.Initialize(this.mockGrfxDevice, (TypeOfShader)123456, null);
+
+        // Assert
+        act.ShouldThrow<ArgumentException>()
+            .Message.ShouldBe($"The '{nameof(TypeOfShader)}.{123456}' is invalid. Could not create vertex and fragment shaders.");
+    }
+
     [Fact]
     public void Initialize_WhenInvoked_InitializesShader()
     {
         // Arrange
+        var onInitInvoked = false;
+        SafeShaderModuleHandle? vertHandle = null;
+        SafeShaderModuleHandle? fragHandle = null;
+
         var sut = CreateSystemUnderTest();
 
         // Act
-        sut.Initialize(this.mockDevice);
-        sut.Initialize(this.mockDevice);
+        sut.Initialize(this.mockGrfxDevice, TypeOfShader.Texture, (vHandle, fHandle) =>
+        {
+            onInitInvoked = true;
+            vertHandle = vHandle;
+            fragHandle = fHandle;
+        });
 
         // Assert
-        this.mockResourceLoader.Received(1).LoadResource($"{ShaderName}.vert.wgsl");
-        this.mockResourceLoader.Received(1).LoadResource($"{ShaderName}.frag.wgsl");
-        this.mockDevice.Received(1).CreateShaderModule("vert source");
-        this.mockDevice.Received(1).CreateShaderModule("frag source");
+        this.mockResourceLoader.Received(1).LoadResource("Texture.vert.wgsl");
+        this.mockResourceLoader.Received(1).LoadResource("Texture.frag.wgsl");
+        this.mockGrfxDevice.Received(1).CreateShaderModule("vert source");
+        this.mockGrfxDevice.Received(1).CreateShaderModule("frag source");
 
-        sut.VertexHandle.ShouldBe(this.vertexHandle);
-        sut.FragmentHandle.ShouldBe(this.fragmentHandle);
+        onInitInvoked.ShouldBeTrue();
+        vertHandle.ShouldBe(this.vertexHandle);
+        fragHandle.ShouldBe(this.fragmentHandle);
     }
 
     [Fact]
@@ -157,39 +121,10 @@ public class GraphicsShaderTests
         var sut = CreateSystemUnderTest();
 
         // Act
-        var act = () => sut.Initialize(null);
+        var act = () => sut.Initialize(null, TypeOfShader.Texture, null);
 
         // Assert
-        act.ShouldThrow<ArgumentNullException>().Message.ShouldBe("Value cannot be null. (Parameter 'gd')");
-    }
-
-    [Fact]
-    public void Dispose_WhenInvoked_DisposesOfShader()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        sut.Initialize(this.mockDevice);
-        sut.Dispose();
-        sut.Dispose();
-
-        // Assert
-        this.mockWgpuInvoker.Received(1).ShaderModuleRelease(UnsafeVertexHandle);
-        this.mockWgpuInvoker.Received(1).ShaderModuleRelease(UnsafeFragmentHandle);
-    }
-
-    [Fact]
-    public void Dispose_WhenNotInitialized_DoesNotThrowException()
-    {
-        // Arrange
-        var sut = CreateSystemUnderTest();
-
-        // Act
-        var act = () => sut.Dispose();
-
-        // Assert
-        act.ShouldNotThrow();
+        act.ShouldThrow<ArgumentNullException>().Message.ShouldBe("Value cannot be null. (Parameter 'grfxDevice')");
     }
     #endregion
 
@@ -197,5 +132,5 @@ public class GraphicsShaderTests
     /// Creates a new instance of <see cref="GraphicsShader"/> for the purpose of testing.
     /// </summary>
     /// <returns>The instance to test.</returns>
-    private GraphicsShader CreateSystemUnderTest() => new (this.mockResourceLoader, this.mockPath, ShaderName);
+    private GraphicsShader CreateSystemUnderTest() => new (this.mockResourceLoader, this.mockPath);
 }
