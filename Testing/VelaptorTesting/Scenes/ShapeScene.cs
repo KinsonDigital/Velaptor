aee90b96d9201a30e7f841e20145f78bfa33f0a9
Calvin Wilkinson <kinsondigital@gmail.com>
2026-09-13 07:09:30 +0100

    refactor: convert code to use object initializers

--- Testing/VelaptorTesting/Scenes/ShapeScene.cs
+++ Testing/VelaptorTesting/Scenes/ShapeScene.cs
@@ -301,19 +301,19 @@ public class ShapeScene : SceneBase
         this.drpShapeType.AddItem(nameof(ShapeType.Rectangle));
         this.drpShapeType.SelectedItemChanged += DrpShapeType_SelectedItemChanged;
 
-        this.layShapeType = new Layout();
-        this.layShapeType.StackDirection = StackDirection.Horizontal;
+        this.layShapeType = new Layout { StackDirection = StackDirection.Horizontal };
         this.layShapeType.AddControl(this.lblShapeType);
         this.layShapeType.AddControl(this.drpShapeType);
 
-        this.chkIsSolid = new CheckBox();
-        this.chkIsSolid.IsChecked = true;
-        this.chkIsSolid.Text = "Solid";
+        this.chkIsSolid = new CheckBox
+        {
+            IsChecked = true,
+            Text = "Solid",
+        };
         this.chkIsSolid.CheckedChanged += ChkShapeIsSolid_CheckedChanged;
 
         // Circle color controls
-        this.lblSolidClr = new Label();
-        this.lblSolidClr.Text = "Solid Color:";
+        this.lblSolidClr = new Label { Text = "Solid Color:" };
 
         this.drpSolidClr = new DropDown();
         this.drpSolidClr.AddItem("Blue");
@@ -322,38 +322,41 @@ public class ShapeScene : SceneBase
         this.drpSolidClr.SelectedItemChanged += DrpSolidClr_SelectedItemChanged;
 
         // Circle color layout
-        this.laySolidClr = new Layout();
-        this.laySolidClr.StackDirection = StackDirection.Horizontal;
-        this.laySolidClr.Centered = true;
+        this.laySolidClr = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+        };
         this.laySolidClr.AddControl(this.lblSolidClr);
         this.laySolidClr.AddControl(this.drpSolidClr);
 
         // Border thickness controls
-        this.lblBorderThickness = new Label();
-        this.lblBorderThickness.Text = "Border Thickness:";
+        this.lblBorderThickness = new Label { Text = "Border Thickness:" };
 
-        this.sldBorderThickness = new Slider();
-        this.sldBorderThickness.Min = 1;
-        this.sldBorderThickness.Max = DefaultCircleDiameter / 2;
+        this.sldBorderThickness = new Slider
+        {
+            Min = 1,
+            Max = DefaultCircleDiameter / 2,
+        };
         this.sldBorderThickness.ValueChanged += SldBorderThickness_ValueChanged;
 
         // Border thickness layout
-        this.layBorderThickness = new Layout();
-        this.layBorderThickness.StackDirection = StackDirection.Horizontal;
-        this.layBorderThickness.Centered = true;
+        this.layBorderThickness = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+        };
         this.layBorderThickness.AddControl(this.lblBorderThickness);
         this.layBorderThickness.AddControl(this.sldBorderThickness);
 
         // Circle properties layout
-        this.layCircleProps = new Layout();
-        this.layCircleProps.StackDirection = StackDirection.Vertical;
+        this.layCircleProps = new Layout { StackDirection = StackDirection.Vertical };
         this.layCircleProps.AddControl(this.chkIsSolid);
         this.layCircleProps.AddControl(this.laySolidClr);
         this.layCircleProps.AddControl(this.layBorderThickness);
 
         // Gradient type controls
-        this.lblGradType = new Label();
-        this.lblGradType.Text = "Gradient Type:";
+        this.lblGradType = new Label { Text = "Gradient Type:" };
 
         this.drpGradType = new DropDown();
         this.drpGradType.AddItem(nameof(ColorGradient.None));
@@ -361,16 +364,17 @@ public class ShapeScene : SceneBase
         this.drpGradType.AddItem(nameof(ColorGradient.Vertical));
         this.drpGradType.SelectedItemChanged += DrpGradType_SelectedItemChanged;
 
-        this.layGradType = new Layout();
-        this.layGradType.StackDirection = StackDirection.Horizontal;
-        this.layGradType.Centered = true;
-        this.layGradType.Enabled = false;
+        this.layGradType = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layGradType.AddControl(this.lblGradType);
         this.layGradType.AddControl(this.drpGradType);
 
         // Gradient start color
-        this.lblGradStartClr = new Label();
-        this.lblGradStartClr.Text = "Gradient Start Color:";
+        this.lblGradStartClr = new Label { Text = "Gradient Start Color:" };
 
         this.drpGradStartClr = new DropDown();
         this.drpGradStartClr.AddItem(nameof(Color.Red));
@@ -378,16 +382,17 @@ public class ShapeScene : SceneBase
         this.drpGradStartClr.AddItem(nameof(Color.Blue));
         this.drpGradStartClr.SelectedItemChanged += DrpGradStartClr_SelectedItemChanged;
 
-        this.layGradStartClr = new Layout();
-        this.layGradStartClr.StackDirection = StackDirection.Horizontal;
-        this.layGradStartClr.Centered = true;
-        this.layGradStartClr.Enabled = false;
+        this.layGradStartClr = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layGradStartClr.AddControl(this.lblGradStartClr);
         this.layGradStartClr.AddControl(this.drpGradStartClr);
 
         // Gradient stop color
-        this.lblGradStopClr = new Label();
-        this.lblGradStopClr.Text = "Gradient Stop Color:";
+        this.lblGradStopClr = new Label { Text = "Gradient Stop Color:" };
 
         this.drpGradStopClr = new DropDown();
         this.drpGradStopClr.SelectedItemChanged += DrpGradStopClr_SelectedItemChanged;
@@ -395,131 +400,161 @@ public class ShapeScene : SceneBase
         this.drpGradStopClr.AddItem(nameof(Color.Red));
         this.drpGradStopClr.AddItem(nameof(Color.Blue));
 
-        this.layGradStopClr = new Layout();
-        this.layGradStopClr.StackDirection = StackDirection.Horizontal;
-        this.layGradStopClr.Centered = true;
-        this.layGradStopClr.Enabled = false;
+        this.layGradStopClr = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layGradStopClr.AddControl(this.lblGradStopClr);
         this.layGradStopClr.AddControl(this.drpGradStopClr);
 
         // Circle diameter
-        this.lblCircleDiameter = new Label();
-        this.lblCircleDiameter.Text = "Diameter:";
+        this.lblCircleDiameter = new Label { Text = "Diameter:" };
 
-        this.sldCircleDiameter = new Slider();
-        this.sldCircleDiameter.Min = 10;
-        this.sldCircleDiameter.Max = 500;
-        this.sldCircleDiameter.Value = DefaultCircleDiameter;
+        this.sldCircleDiameter = new Slider
+        {
+            Min = 10,
+            Max = 500,
+            Value = DefaultCircleDiameter,
+        };
         this.sldCircleDiameter.ValueChanged += SldCircleDiameter_ValueChanged;
 
-        this.layCircleDiameter = new Layout();
-        this.layCircleDiameter.StackDirection = StackDirection.Horizontal;
-        this.layCircleDiameter.Centered = true;
+        this.layCircleDiameter = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+        };
         this.layCircleDiameter.AddControl(this.lblCircleDiameter);
         this.layCircleDiameter.AddControl(this.sldCircleDiameter);
 
         // Rectangle width
-        this.lblRectWidth = new Label();
-        this.lblRectWidth.Text = "Width:";
+        this.lblRectWidth = new Label { Text = "Width:" };
 
-        this.sldRectWidth = new Slider();
-        this.sldRectWidth.Value = DefaultRectWidth;
-        this.sldRectWidth.Min = MinRectWidth;
-        this.sldRectWidth.Max = MaxRectHeight;
+        this.sldRectWidth = new Slider
+        {
+            Value = DefaultRectWidth,
+            Min = MinRectWidth,
+            Max = MaxRectHeight,
+        };
         this.sldRectWidth.ValueChanged += SldRectWidth_ValueChanged;
 
-        this.layRectWidth = new Layout();
-        this.layRectWidth.StackDirection = StackDirection.Horizontal;
-        this.layRectWidth.Enabled = false;
-        this.layRectWidth.Centered = true;
+        this.layRectWidth = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Enabled = false,
+            Centered = true,
+        };
         this.layRectWidth.AddControl(this.lblRectWidth);
         this.layRectWidth.AddControl(this.sldRectWidth);
 
-        this.lblRectHeight = new Label();
-        this.lblRectHeight.Text = "Height:";
+        this.lblRectHeight = new Label { Text = "Height:" };
 
         // Rectangle height
-        this.sldRectHeight = new Slider();
-        this.sldRectHeight.Value = DefaultRectHeight;
-        this.sldRectHeight.Min = MinRectHeight;
-        this.sldRectHeight.Max = MaxRectHeight;
+        this.sldRectHeight = new Slider
+        {
+            Value = DefaultRectHeight,
+            Min = MinRectHeight,
+            Max = MaxRectHeight,
+        };
         this.sldRectHeight.ValueChanged += SldRectHeight_ValueChanged;
 
-        this.layRectHeight = new Layout();
-        this.layRectHeight.Name = "debug";
-        this.layRectHeight.StackDirection = StackDirection.Horizontal;
-        this.layRectHeight.Enabled = false;
-        this.layRectHeight.Centered = true;
+        this.layRectHeight = new Layout
+        {
+            Name = "debug",
+            StackDirection = StackDirection.Horizontal,
+            Enabled = false,
+            Centered = true,
+        };
         this.layRectHeight.AddControl(this.lblRectHeight);
         this.layRectHeight.AddControl(this.sldRectHeight);
 
         // Bottom left radius
-        this.lblBottomLeftRadius = new Label();
-        this.lblBottomLeftRadius.Text = "Bottom Left:";
+        this.lblBottomLeftRadius = new Label { Text = "Bottom Left:" };
 
-        this.sldBottomLeftRadius = new Slider();
-        this.sldBottomLeftRadius.Min = 0;
-        this.sldBottomLeftRadius.Max = this.rectangle.Width < this.rectangle.Height ? this.rectangle.Width : this.rectangle.Height;
+        this.sldBottomLeftRadius = new Slider
+        {
+            Min = 0,
+            Max = this.rectangle.Width < this.rectangle.Height
+                ? this.rectangle.Width
+                : this.rectangle.Height,
+        };
         this.sldBottomLeftRadius.ValueChanged += SldBottomLeftRadius_ValueChanged;
 
-        this.layBottomLeftRadius = new Layout();
-        this.layBottomLeftRadius.StackDirection = StackDirection.Horizontal;
-        this.layBottomLeftRadius.Centered = true;
-        this.layBottomLeftRadius.Enabled = false;
+        this.layBottomLeftRadius = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layBottomLeftRadius.AddControl(this.lblBottomLeftRadius);
         this.layBottomLeftRadius.AddControl(this.sldBottomLeftRadius);
 
         // Bottom right radius
-        this.lblBottomRightRadius = new Label();
-        this.lblBottomRightRadius.Text = "Bottom Right:";
+        this.lblBottomRightRadius = new Label { Text = "Bottom Right:" };
 
-        this.sldBottomRightRadius = new Slider();
-        this.sldBottomRightRadius.Min = 0;
-        this.sldBottomRightRadius.Max = this.rectangle.Width < this.rectangle.Height ? this.rectangle.Width : this.rectangle.Height;
+        this.sldBottomRightRadius = new Slider
+        {
+            Min = 0,
+            Max = this.rectangle.Width < this.rectangle.Height
+                ? this.rectangle.Width
+                : this.rectangle.Height,
+        };
         this.sldBottomRightRadius.ValueChanged += SldBottomRightRadius_ValueChanged;
 
-        this.layBottomRightRadius = new Layout();
-        this.layBottomRightRadius.StackDirection = StackDirection.Horizontal;
-        this.layBottomRightRadius.Centered = true;
-        this.layBottomRightRadius.Enabled = false;
+        this.layBottomRightRadius = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layBottomRightRadius.AddControl(this.lblBottomRightRadius);
         this.layBottomRightRadius.AddControl(this.sldBottomRightRadius);
 
         // Top right radius
-        this.lblTopRightRadius = new Label();
-        this.lblTopRightRadius.Text = "Top Right:";
+        this.lblTopRightRadius = new Label { Text = "Top Right:" };
 
-        this.sldTopRightRadius = new Slider();
-        this.sldTopRightRadius.Min = 0;
-        this.sldTopRightRadius.Max = this.rectangle.Width < this.rectangle.Height ? this.rectangle.Width : this.rectangle.Height;
+        this.sldTopRightRadius = new Slider
+        {
+            Min = 0,
+            Max = this.rectangle.Width < this.rectangle.Height
+                ? this.rectangle.Width
+                : this.rectangle.Height,
+        };
         this.sldTopRightRadius.ValueChanged += SldTopRightRadius_ValueChanged;
 
-        this.layTopRightRadius = new Layout();
-        this.layTopRightRadius.StackDirection = StackDirection.Horizontal;
-        this.layTopRightRadius.Centered = true;
-        this.layTopRightRadius.Enabled = false;
+        this.layTopRightRadius = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layTopRightRadius.AddControl(this.lblTopRightRadius);
         this.layTopRightRadius.AddControl(this.sldTopRightRadius);
 
         // Top left radius
-        this.lblTopLeftRadius = new Label();
-        this.lblTopLeftRadius.Text = "Top Left:";
+        this.lblTopLeftRadius = new Label { Text = "Top Left:" };
 
-        this.sldTopLeftRadius = new Slider();
-        this.sldTopLeftRadius.Min = 0;
-        this.sldTopLeftRadius.Max = this.rectangle.Width < this.rectangle.Height ? this.rectangle.Width : this.rectangle.Height;
+        this.sldTopLeftRadius = new Slider
+        {
+            Min = 0,
+            Max = this.rectangle.Width < this.rectangle.Height
+                ? this.rectangle.Width
+                : this.rectangle.Height,
+        };
         this.sldTopLeftRadius.ValueChanged += SldTopLeftRadius_ValueChanged;
 
-        this.layTopLeftRadius = new Layout();
-        this.layTopLeftRadius.StackDirection = StackDirection.Horizontal;
-        this.layTopLeftRadius.Centered = true;
-        this.layTopLeftRadius.Enabled = false;
+        this.layTopLeftRadius = new Layout
+        {
+            StackDirection = StackDirection.Horizontal,
+            Centered = true,
+            Enabled = false,
+        };
         this.layTopLeftRadius.AddControl(this.lblTopLeftRadius);
         this.layTopLeftRadius.AddControl(this.sldTopLeftRadius);
 
         // Create main layout for the other layouts
-        this.layMain = new Layout();
-        this.layMain.Name = "Main Layout";
+        this.layMain = new Layout { Name = "Main Layout" };
         this.layMain.AddControl(this.layShapeType);
         this.layMain.AddControl(this.layCircleProps);
         this.layMain.AddControl(this.layGradType);
@@ -534,9 +569,12 @@ public class ShapeScene : SceneBase
         this.layMain.AddControl(this.layTopLeftRadius);
 
         // Create main container
-        this.conMain = new Container();
-        this.conMain.Title = "Shape Settings";
-        this.conMain.Position = new Vector2(15, 15);
+        this.conMain = new Container
+        {
+            Title = "Shape Settings",
+            Position = new Vector2(15,
+                15),
+        };
         this.conMain.AddLayoutControl(this.layMain);
     }
 
